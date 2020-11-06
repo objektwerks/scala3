281624cc70f0f8a69d0000ff9ba4eca4e49daacc
objektwerks <objektwerks@runbox.com>
2020-11-06 10:12:07 -0500

    edited intersection and union type tests

--- src/test/scala/objektwerks/UnionTypeTest.scala
+++ src/test/scala/objektwerks/UnionTypeTest.scala
@@ -2,21 +2,20 @@ package objektwerks
 
 import munit._
 
-trait Drink {
-  def drink(): Beer
-}
-
 trait Beer {
   val name: String
-  def drink: Beer = this
 }
 
-case class Lager(name: String) extends Beer
-case class Pilsner(name: String) extends Beer
+case class Lager(name: String) extends Beer {
+  def prost: Lager = this
+}
+case class Pilsner(name: String) extends Beer {
+  def cheers: Pilsner = this
+}
 
 def drink(beer: Lager | Pilsner): Beer = beer match {
-  case lager @ Lager(name) => lager.drink
-  case pilsner @ Pilsner(name) => pilsner.drink
+  case lager @ Lager(name) => lager.prost
+  case pilsner @ Pilsner(name) => pilsner.cheers
 }
 
 class UnionTypeTest extends FunSuite {
