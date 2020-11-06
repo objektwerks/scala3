67cc9ae7df805c972a80fd62037c68c5be9f3361
objektwerks <objektwerks@runbox.com>
2020-11-06 06:51:50 -0500

    added intersection type test

---
+++ src/test/scala/objektwerks/IntersectionTypeTest.scala
@@ -0,0 +1,23 @@
+package objektwerks
+
+import munit._
+
+trait Camera {
+    def takePhoto(): Boolean = true
+}
+
+trait Phone {
+    def makeCall(): Boolean = true
+}
+
+class MobilePhone extends Camera with Phone
+
+def useDevice(device: Camera & Phone): (Boolean, Boolean) = {
+    ( device.takePhoto(), device.makeCall() )
+}
+
+class IntersectionTypeTest extends FunSuite {
+  test("intersection") {
+    assert( useDevice( MobilePhone() ) == (true, true) )
+  }
+}
