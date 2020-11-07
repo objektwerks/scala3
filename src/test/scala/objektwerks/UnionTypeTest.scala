d07f573e35cfb293714579b11d01450d97bf942b
objektwerks <objektwerks@runbox.com>
2020-11-07 07:23:09 -0500

    edited union test

--- src/test/scala/objektwerks/UnionTypeTest.scala
+++ src/test/scala/objektwerks/UnionTypeTest.scala
@@ -2,16 +2,9 @@ package objektwerks
 
 import munit._
 
-trait Beer {
-  val name: String
-}
-
-case class Lager(name: String) extends Beer {
-  def prost: Lager = this
-}
-case class Pilsner(name: String) extends Beer {
-  def cheers: Pilsner = this
-}
+sealed trait Beer { val name: String }
+final case class Lager(name: String) extends Beer { def prost: Lager = this }
+final case class Pilsner(name: String) extends Beer { def cheers: Pilsner = this }
 
 def drink(beer: Lager | Pilsner): Beer = beer match {
   case lager @ Lager(name) => lager.prost
