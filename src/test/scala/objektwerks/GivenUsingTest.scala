84edab0d959974b2c146b489870456c5734e3af1
objektwerks <objektwerks@runbox.com>
2020-11-08 07:44:25 -0500

    implemented given-using test

--- src/test/scala/objektwerks/GivenUsingTest.scala
+++ src/test/scala/objektwerks/GivenUsingTest.scala
@@ -2,8 +2,23 @@ package objektwerks
 
 import munit._
 
+trait Combiner[T] {
+  def combine(list: List[T]): T
+}
+
+given IntCombiner as Combiner[Int] {
+  def combine(list: List[Int]): Int = list.sum
+}
+
+given StringCombiner as Combiner[String] {
+  def combine(list: List[String]): String = list.mkString("")
+}
+
+def combineList[T](list: List[T])(using combiner: Combiner[T]): T = combiner.combine(list)
+
 class GivenUsingTest extends FunSuite {
   test("given > using") {
-    
+    assert( combineList( List(1, 2, 3) ) == 6 )
+    assert( combineList( List("Scala ", "is ", "awesome!") ) == "Scala is awesome!" )
   }
 }
