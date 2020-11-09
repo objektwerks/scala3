ce069fb4b3d070cafb1cdf1bbd6077fad3e55b11
objektwerks <objektwerks@runbox.com>
2020-11-09 07:10:30 -0500

    enhanced infix test

--- src/test/scala/objektwerks/InfixTest.scala
+++ src/test/scala/objektwerks/InfixTest.scala
@@ -16,6 +16,10 @@ class InfixTest extends FunSuite {
     assert( Amount(1.0) + Amount(2.0) == Amount(3.0) )
     assert( Amount(3.0) - Amount(2.0) == Amount(1.0) )
 
+    val added = Amount(1.0) add Amount(2.0)
+    val subtracted = Amount(3.0) subtract Amount(2.0)
+    assert( added == Amount(3.0) )
+    assert( subtracted == Amount(1.0) )
     assert( (Amount(1.0) add Amount(2.0)) == Amount(3.0) )
     assert( (Amount(3.0) subtract Amount(2.0)) == Amount(1.0) )
   }
