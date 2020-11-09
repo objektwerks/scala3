f061d665ca3570ed7b9a7097292b79951eedd226
objektwerks <objektwerks@runbox.com>
2020-11-09 07:06:48 -0500

    added infix test

---
+++ src/test/scala/objektwerks/InfixTest.scala
@@ -0,0 +1,22 @@
+package objektwerks
+
+import munit._
+
+import scala.annotation.infix
+
+case class Amount(amount: Double) {
+  def +(other: Amount): Amount = Amount(amount + other.amount)
+  def -(other: Amount): Amount = Amount(amount - other.amount)
+  @infix def add(other: Amount): Amount = this + other
+  @infix def subtract(other: Amount): Amount = this - other
+
+}
+class InfixTest extends FunSuite {
+  test("infix") {
+    assert( Amount(1.0) + Amount(2.0) == Amount(3.0) )
+    assert( Amount(3.0) - Amount(2.0) == Amount(1.0) )
+
+    assert( (Amount(1.0) add Amount(2.0)) == Amount(3.0) )
+    assert( (Amount(3.0) subtract Amount(2.0)) == Amount(1.0) )
+  }
+}
