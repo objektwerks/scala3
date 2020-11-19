135ab7e11a31c7a2ba9469063a5730dff5548dc1
objektwerks <objektwerks@runbox.com>
2020-11-19 06:44:37 -0500

    corrected opaque type test

--- src/test/scala/objektwerks/OpaqueTest.scala
+++ src/test/scala/objektwerks/OpaqueTest.scala
@@ -2,18 +2,13 @@ package objektwerks
 
 import munit._
 
-object Names {
-  opaque type Name = String
-
-  object Name {
-    def apply(name: String): Name = name
-  }
+opaque type Name = String
+object Name {
+  def apply(name: String): Name = name
 }
 
 class OpaqueTest extends FunSuite {
   test("opaque") {
-    import Names._
-
     val fredFlintstone = Name("Fred Flintstone")
     assert( fredFlintstone == Name("Fred Flintstone") )
   }
