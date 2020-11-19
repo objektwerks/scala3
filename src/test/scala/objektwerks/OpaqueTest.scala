81ce3abfff8661e9ad16bce61fe13d873e5495de
objektwerks <objektwerks@runbox.com>
2020-11-19 08:11:24 -0500

    enhanced opaque type test

--- src/test/scala/objektwerks/OpaqueTest.scala
+++ src/test/scala/objektwerks/OpaqueTest.scala
@@ -7,6 +7,8 @@ object Pulse {
   def apply(pulse: Int): Pulse = pulse
 }
 extension (pulse: Pulse) {
+  def asInt: Int = pulse.toInt
+  def asIntOption: Option[Pulse] = if (pulse > 0) Some(pulse) else None
   def asJson: String = """
                          { "pulse": 56 }
                        """
@@ -16,6 +18,8 @@ class OpaqueTest extends FunSuite {
   test("opaque") {
     val pulse = Pulse(56)
     assert( pulse == Pulse(56) )
+    assert( pulse.asInt == 56 )
+    assert( pulse.asIntOption == Some(pulse) )
     assert( pulse.asJson.contains("56") )
   }
 }
