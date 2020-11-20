09e70c4bc270c460306afccc8fcfcb52e035105c
objektwerks <objektwerks@runbox.com>
2020-11-20 12:35:38 -0500

    added conversion test

---
+++ src/test/scala/objektwerks/ConversionTest.scala
@@ -0,0 +1,19 @@
+package objektwerks
+
+import munit._
+
+case class Person(name: String) {
+  def greetings: String = s"Greetings. My name is $name."
+}
+
+given stringToPerson as Conversion[String, Person] {
+  def apply(name: String): Person = Person(name)
+}
+
+class ConversionTest extends FunSuite {
+  test("conversion") {
+    import scala.language.implicitConversions
+    
+    assert("Fred Flintstone".greetings == "Greetings. My name is Fred Flintstone.")
+  }
+}
