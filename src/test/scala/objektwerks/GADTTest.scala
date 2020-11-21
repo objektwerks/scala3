e31cc4c0a97062e9737adc8c71ef666539b1680d
objektwerks <objektwerks@runbox.com>
2020-11-21 08:38:17 -0500

    enhanced gadt test

--- src/test/scala/objektwerks/GADTTest.scala
+++ src/test/scala/objektwerks/GADTTest.scala
@@ -3,8 +3,8 @@ package objektwerks
 import munit._
 
 sealed trait Expression[T]
-final case class IntExpr(i: Int) extends Expression[Int]
-final case class BoolExpr(b: Boolean) extends Expression[Boolean]
+final case class IntExpr(int: Int) extends Expression[Int]
+final case class BoolExpr(boolean: Boolean) extends Expression[Boolean]
 
 def eval[T](expression: Expression[T]): T = expression match {
   case IntExpr(int) => int
