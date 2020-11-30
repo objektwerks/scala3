6d4b53f00ad87660795027b3e5889a51da022539
objektwerks <objektwerks@runbox.com>
2020-11-30 11:44:53 -0500

    refactored partial function test

--- src/test/scala/objektwerks/PartialFunctionTest.scala
+++ src/test/scala/objektwerks/PartialFunctionTest.scala
@@ -4,11 +4,11 @@ import munit._
 
 import scala.util.Try
 
-val divideByOne: PartialFunction[Int, Int] = {
-  case i: Int if i != 0 => i / 1
+val multipleByOne: PartialFunction[Int, Int] = {
+  case i: Int if i != 0 => i * 1
 }
 
-val divideByOneExt = new PartialFunction[Int, Int] {
+val divideByOne = new PartialFunction[Int, Int] {
   def apply(i: Int): Int = i / 1
   def isDefinedAt(i: Int): Boolean = i != 0
 }
@@ -23,14 +23,14 @@ def isOdd: PartialFunction[Int, String] = {
 
 class PartialFunctionTest extends FunSuite {
   test("partial function") {
-    assert( ( Try { List(0, 1, 2) map divideByOne }.isFailure ) == true )
-    assert( ( List(0, 1, 2) collect divideByOne ) == List(1, 2) )
-    assert( ( List(42, "cat") collect { case i: Int => divideByOne(i) } ) == List(42) )
+    assert( ( Try { List(0, 1, 2) map multipleByOne }.isFailure ) == true )
+    assert( ( List(0, 1, 2) collect multipleByOne ) == List(1, 2) )
+    assert( ( List(42, "cat") collect { case i: Int => multipleByOne(i) } ) == List(42) )
 
-    assert( divideByOneExt(2) == 2 )
-    assert( divideByOneExt(0) == 0 )
-    assert( divideByOneExt.isDefinedAt(3) == true )
-    assert( divideByOneExt.isDefinedAt(0) == false )
+    assert( divideByOne(2) == 2 )
+    assert( divideByOne(0) == 0 )
+    assert( divideByOne.isDefinedAt(3) == true )
+    assert( divideByOne.isDefinedAt(0) == false )
 
     assert( ( 1 to 3 collect isEven ) == Vector("2 even") )
     assert( ( 1 to 3 collect isOdd ) == Vector("1 odd", "3 odd") )
