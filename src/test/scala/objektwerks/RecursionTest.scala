465b75970c852e36326d98ffc37d81bd90f7dbea
objektwerks <objektwerks@runbox.com>
2020-12-16 08:11:04 -0500

    refactored RecursionTest to use scalatest

--- src/test/scala/objektwerks/RecursionTest.scala
+++ src/test/scala/objektwerks/RecursionTest.scala
@@ -1,6 +1,7 @@
 package objektwerks
 
-import munit._
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
 
 import scala.annotation.tailrec
 import scala.util.Try
@@ -58,40 +59,40 @@ final def intersectLists[A](listA: List[A],
       else intersectLists(tail, listB, acc)
   }
 
-class RecursionTest extends FunSuite {
+class RecursionTest extends AnyFunSuite with Matchers {
   test("sum") {
-    assert( sum( Nil ) == 0 )
-    assert( sum( List(1, 2, 3) ) == 6 )
+    sum( Nil ) shouldBe 0
+    sum( List(1, 2, 3) ) shouldBe 6
   }
 
   test("product") {
-    assert( product( Nil ) == 1 )
-    assert( product( List(1, 2, 3) ) == 6 )
+    product( Nil ) shouldBe 1
+    product( List(1, 2, 3) ) shouldBe 6
   }
 
   test("reverse") {
-    assert( reverse( List(1, 2, 3) ) == List(3, 2, 1) )
+    reverse( List(1, 2, 3) ) shouldBe List(3, 2, 1)
   }
 
   test("find nth element from right") {
     val xs = (1 to 10).toList
-    assert( findNthElementFromRight(xs, 4) == Some(7) )
-    assert( findNthElementFromRight(xs, 15) == None )
+    findNthElementFromRight(xs, 4) shouldBe Some(7)
+    findNthElementFromRight(xs, 15) shouldBe None
   }
 
   test("factorial") {
-    assert( factorial(3) == 6 )
+    factorial(3) shouldBe 6
   }
 
   test("fibbonaci") {
-    assert( fibonacci(39) == 63245986 )
+    fibonacci(39) shouldBe 63245986
   }
 
   test("intersect lists") {
     val listA = (1 to 10).toList
     val listB = (5 to 15).toList
     val listIntersection = List(5, 6, 7, 8, 9, 10)
-    assert( intersectLists(listA, listB) == listIntersection )
-    assert( ( listA intersect listB ) == listIntersection )
+    intersectLists(listA, listB) shouldBe listIntersection
+    ( listA intersect listB ) shouldBe listIntersection
   }
 }
