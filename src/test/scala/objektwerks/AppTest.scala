f12fc36d74f5cab367d6744c7912f9a3d8bb6085
objektwerks <objektwerks@runbox.com>
2020-12-25 15:12:12 -0500

    added scalatest; converted app test

--- src/test/scala/objektwerks/AppTest.scala
+++ src/test/scala/objektwerks/AppTest.scala
@@ -1,9 +1,10 @@
 package objektwerks
 
-import munit._
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
 
-class AppTest extends FunSuite {
+class AppTest extends AnyFunSuite with Matchers {
   test("app") {
-    assert(App.title.nonEmpty)
+    App.title.nonEmpty shouldBe true
   }
 }
