16c99fbb6f40775a93084cc12755f5f3f959796e
objektwerks <objektwerks@runbox.com>
2020-12-31 08:31:22 -0500

    refactored munit to scalatest

--- src/test/scala/objektwerks/OptionTest.scala
+++ src/test/scala/objektwerks/OptionTest.scala
@@ -1,33 +1,34 @@
 package objektwerks
 
-import munit._
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
 
 import scala.util.Try
 
 def isGreaterThanZero(x: Int): Option[Int] = if (x > 0) Some(x) else None
 
-class OptionTest extends FunSuite {
+class OptionTest extends AnyFunSuite with Matchers {
   test("option") {
-    assert( isGreaterThanZero(1).get == 1 )
-    assert( isGreaterThanZero(0).getOrElse(1) == 1 )
-    assert( ( isGreaterThanZero(0) orElse Some(-1) contains -1 ) == true )
+    isGreaterThanZero(1).get shouldBe 1
+    isGreaterThanZero(0).getOrElse(1) shouldBe 1
+    ( isGreaterThanZero(0) orElse Some(-1) contains -1 ) shouldBe true
 
-    assert( isGreaterThanZero(0).isEmpty == true )
-    assert( isGreaterThanZero(1).nonEmpty == true )
-    assert( isGreaterThanZero(1).isDefined == true )
+    isGreaterThanZero(0).isEmpty shouldBe true
+    isGreaterThanZero(1).nonEmpty shouldBe true
+    isGreaterThanZero(1).isDefined shouldBe true
 
-    assert( ( isGreaterThanZero(1) collect { case n: Int => n * 3 } contains 3 ) == true )
+    ( isGreaterThanZero(1) collect { case n: Int => n * 3 } contains 3 ) shouldBe true
 
-    assert( isGreaterThanZero(1).contains(1) == true )
-    assert( isGreaterThanZero(1).count(_ > 0) == 1 )
+    isGreaterThanZero(1).contains(1) shouldBe true
+    isGreaterThanZero(1).count(_ > 0) shouldBe 1
 
-    assert( isGreaterThanZero(1).exists(_ > 0) == true )
-    assert( ( isGreaterThanZero(1).filter(_ > 0) contains 1 ) == true )
+    isGreaterThanZero(1).exists(_ > 0) shouldBe true
+    ( isGreaterThanZero(1).filter(_ > 0) contains 1 ) shouldBe true
 
-    assert( isGreaterThanZero(1).forall(_ > 0) == true )
-    isGreaterThanZero(3) foreach { v => assert( v == 3 ) }
+    isGreaterThanZero(1).forall(_ > 0) shouldBe true
+    isGreaterThanZero(3) foreach { v => v shouldBe 3 }
 
-    assert( isGreaterThanZero(1).fold(1)(_ * 3) == 3 )
+    isGreaterThanZero(1).fold(1)(_ * 3) shouldBe 3
   }
 
   test("match") {
@@ -35,23 +36,23 @@ class OptionTest extends FunSuite {
       case Some(n) => n * 3
       case None => -1
     }
-    assert( x == 3 )
+    x shouldBe 3
   }
 
   test("map") {
-    assert( ( isGreaterThanZero(1) map(_ * 3) getOrElse(-1) ) == 3 )
+    ( isGreaterThanZero(1) map(_ * 3) getOrElse(-1) ) shouldBe 3
   }
 
-  test("flatMap") {
+  test("flatmap") {
     def toInt(s: String): Option[Int] = s.toIntOption
 
     val strings = List("1", "2", "3", "four")
-    assert( strings.flatMap(toInt) == List(1, 2, 3) )
-    assert( strings.flatMap(toInt).sum == 6 )
+    strings.flatMap(toInt) shouldBe List(1, 2, 3)
+    strings.flatMap(toInt).sum shouldBe 6
 
     def sum(x: Option[Int], y: Option[Int]): Option[Int] = x.flatMap(i => y.map(j => i + j))
-
-    assert( sum(toInt("1"), toInt("2")).contains(3) == true )
-    assert( sum(toInt("1"), toInt("zspace")).isEmpty == true )
+    
+    sum(toInt("1"), toInt("2")).contains(3) shouldBe true
+    sum(toInt("1"), toInt("zspace")).isEmpty shouldBe true
   }
 }
