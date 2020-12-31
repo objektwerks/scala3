146b0a0602e752c8d3c6b0da7404c2f012ec7e68
objektwerks <objektwerks@runbox.com>
2020-12-31 08:47:23 -0500

    refactored munit to scalatest

--- src/test/scala/objektwerks/UntuplingTest.scala
+++ src/test/scala/objektwerks/UntuplingTest.scala
@@ -1,13 +1,14 @@
 package objektwerks
 
-import munit._
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
 
-class UntuplingTest extends FunSuite {
+class UntuplingTest extends AnyFunSuite with Matchers {
   test("untupling") {
     val tuples = List( (1, 1), (2, 2), (3, 3) )
     val sums = tuples.map {
       (x, y) => x + y
     }
-    assert( sums == List(2, 4, 6) )
+    sums shouldBe List(2, 4, 6)
   }
 }
