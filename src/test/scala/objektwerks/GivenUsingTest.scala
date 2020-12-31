8dbd3dc186f97155dfdaaaa899fffe15a1c75aff
objektwerks <objektwerks@runbox.com>
2020-12-31 08:15:37 -0500

    refactored munit to scalatest

--- src/test/scala/objektwerks/GivenUsingTest.scala
+++ src/test/scala/objektwerks/GivenUsingTest.scala
@@ -1,6 +1,7 @@
 package objektwerks
 
-import munit._
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
 
 trait Combiner[T] {
   def combine(list: List[T]): T
@@ -14,9 +15,9 @@ given Combiner[String] with
 
 def combineList[T](list: List[T])(using combiner: Combiner[T]): T = combiner.combine(list)
 
-class GivenUsingTest extends FunSuite {
+class GivenUsingTest extends AnyFunSuite with Matchers {
   test("given > using") {
-    assert( combineList( List(1, 2, 3) ) == 6 )
-    assert( combineList( List("Scala3 ", "is a ", "new language!") ) == "Scala3 is a new language!" )
+    combineList( List(1, 2, 3) ) shouldBe 6
+    combineList( List("Scala3 ", "is a ", "new language!") ) shouldBe "Scala3 is a new language!"
   }
 }
