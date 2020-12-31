b524bf79832c880f0ff6e28f6d2c931dd5272f9d
objektwerks <objektwerks@runbox.com>
2020-12-31 08:46:09 -0500

    refactored munit to scalatest

--- src/test/scala/objektwerks/UnionTypeTest.scala
+++ src/test/scala/objektwerks/UnionTypeTest.scala
@@ -1,6 +1,7 @@
 package objektwerks
 
-import munit._
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
 
 sealed trait Beer { val name: String }
 final case class Lager(name: String) extends Beer { def prost: Lager = this }
@@ -11,15 +12,15 @@ def drink(beer: Lager | Pilsner): Beer = beer match {
   case pilsner @ Pilsner(name) => pilsner.cheers
 }
 
-class UnionTypeTest extends FunSuite {
+class UnionTypeTest extends AnyFunSuite with Matchers {
   test("union") {
     val lager = Lager("Song of Joy")
     val pilsner = Pilsner("Poetica 2")
 
-    assert( drink( lager ).isInstanceOf[Lager] )
-    assert( drink( lager ).name == "Song of Joy" )
+    drink( lager ).isInstanceOf[Lager] shouldBe true
+    drink( lager ).name shouldBe "Song of Joy"
 
-    assert( drink( pilsner ).isInstanceOf[Pilsner] )
-    assert( drink( pilsner ).name == "Poetica 2" )
+    drink( pilsner ).isInstanceOf[Pilsner] shouldBe true
+    drink( pilsner ).name shouldBe "Poetica 2"
   }
 }
