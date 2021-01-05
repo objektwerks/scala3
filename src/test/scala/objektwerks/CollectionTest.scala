22a058630faaa7e5d1a94359d13de1ce4fa73120
objektwerks <objektwerks@runbox.com>
2021-01-05 09:04:32 -0500

    enhanced newBuilder test

--- src/test/scala/objektwerks/CollectionTest.scala
+++ src/test/scala/objektwerks/CollectionTest.scala
@@ -3,6 +3,7 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
+import scala.collection.View
 import scala.collection.concurrent
 import scala.collection.immutable._
 import scala.collection.mutable
@@ -12,12 +13,13 @@ class CollectionTest extends AnyFunSuite with Matchers {
   def toList(i: Int): List[Int] = List(i - 1, i, i + 1)
 
   test("newBuilder") {
-    Array.newBuilder[Int].addOne(1).addOne(2).addOne(3).result() shouldBe Array(1, 2, 3)
-    Seq.newBuilder[Int].addOne(1).addOne(2).addOne(3).result() shouldBe Seq(1, 2, 3)
-    IndexedSeq.newBuilder[Int].addOne(1).addOne(2).addOne(3).result() shouldBe IndexedSeq(1, 2, 3)
-    LinearSeq.newBuilder[Int].addOne(1).addOne(2).addOne(3).result() shouldBe LinearSeq(1, 2, 3)
-    Set.newBuilder[Int].addOne(1).addOne(2).addOne(3).result() shouldBe Set(1, 2, 3)
-    Map.newBuilder[Int, Int].addOne(1 -> 1).addOne(2 -> 2).addOne(3 -> 3).result() shouldBe Map(1 -> 1, 2 -> 2, 3 -> 3)
+    Array.newBuilder[Int].addOne(1).addOne(2).addOne(3).result shouldBe Array(1, 2, 3)
+    Seq.newBuilder[Int].addOne(1).addOne(2).addOne(3).result shouldBe Seq(1, 2, 3)
+    IndexedSeq.newBuilder[Int].addOne(1).addOne(2).addOne(3).result shouldBe IndexedSeq(1, 2, 3)
+    LinearSeq.newBuilder[Int].addOne(1).addOne(2).addOne(3).result shouldBe LinearSeq(1, 2, 3)
+    Set.newBuilder[Int].addOne(1).addOne(2).addOne(3).result shouldBe Set(1, 2, 3)
+    Map.newBuilder[Int, Int].addOne(1 -> 1).addOne(2 -> 2).addOne(3 -> 3).result shouldBe Map(1 -> 1, 2 -> 2, 3 -> 3)
+    View.newBuilder[Int].addOne(1).addOne(2).addOne(3).result.toList shouldBe List(1, 2, 3)
   }
 
   test("list") {
