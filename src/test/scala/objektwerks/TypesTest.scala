1112fd4a7167e17b1bf77c6dfd24ddd8e9a3ec3a
objektwerks <objektwerks@runbox.com>
2021-01-10 08:02:45 -0500

    enhanced types test

--- src/test/scala/objektwerks/TypesTest.scala
+++ src/test/scala/objektwerks/TypesTest.scala
@@ -7,35 +7,57 @@ import org.scalatest.matchers.should.Matchers
 sealed trait Relative
 class Parent extends Relative
 class Child extends Parent
+
 class Covariant[+R](val relative: R)
 class Contravariant[-R, +S](val relative: S)
 class Invariant[R](val relative: R)
-trait PositiveFilter[-A, +B] { def isPositive(n: Int): Boolean }
+
+trait PositiveFilter[-A, +B] { 
+  def isPositive(n: Int): Boolean 
+}
 
 // Bounds
-object UpperBounds { def apply[U <: AnyVal](n: U): U = identity(n) }
-object LowerBounds { def apply[L >: AnyVal](n: L): L = identity(n) }
-
-// Compound Types
-trait Init { def init: Boolean = true }
-trait Run extends Init { def run: Boolean = init }
-class Runnable extends Run {
-  def isRunning: Boolean = run
+object UpperBounds { 
+  def apply[U <: AnyVal](n: U): U = identity(n) 
 }
-trait Emotion { def isEmoting: Boolean = true }
-trait Speach { def isSpeaking: Boolean = true }
+object LowerBounds { 
+  def apply[L >: AnyVal](n: L): L = identity(n) 
+}
+
+// Compound Type
+trait Init { 
+  def init: Boolean = true 
+}
+trait Run extends Init { 
+  def run: Boolean = init
+}
+class Runnable extends Run { 
+  def isRunning: Boolean = run 
+}
+trait Emotion { 
+  def isEmoting: Boolean = true 
+}
+trait Speach { 
+  def isSpeaking: Boolean = true 
+}
 class Robot extends Runnable with Emotion with Speach
 
 // Self Type
-trait Speaking { def speaking: String }
-trait Hello extends Speaking { override def speaking = "hello" }
-trait Goodbye extends Speaking { override def speaking = "goodbye" }
+trait Speaking { 
+  def speaking: String 
+}
+trait Hello extends Speaking { 
+  override def speaking = "hello" 
+}
+trait Goodbye extends Speaking { 
+  override def speaking = "goodbye" 
+}
 class Speaker {
   self: Speaking =>
   def speak: String = speaking
 }
 
-// Path Dependent Types
+// Path Dependent Type
 class First {
   class Second
 }
@@ -45,6 +67,7 @@ class TypesTest extends AnyFunSuite with Matchers {
     val covariant: Covariant[Parent] = Covariant[Child](Child())
     val contravariant: Contravariant[Child, Parent] = Contravariant[Child, Parent](Parent())
     val invariant: Invariant[Child] = Invariant[Child](Child())
+
     covariant.relative.isInstanceOf[Child] shouldBe true
     contravariant.relative.isInstanceOf[Parent] shouldBe true
     invariant.relative.isInstanceOf[Child] shouldBe true
@@ -54,8 +77,10 @@ class TypesTest extends AnyFunSuite with Matchers {
     val filter = new PositiveFilter[Int, Boolean] {
       override def isPositive(n: Int): Boolean = n > 0
     }
+
     val numbers = List(-3, -2, -1, 0, 1, 2, 3)
-    val positives: List[Int] = numbers.filter(n => filter.isPositive(n))
+    val positives = numbers.filter(n => filter.isPositive(n))
+
     positives shouldEqual List(1, 2, 3)
   }
 
@@ -67,7 +92,7 @@ class TypesTest extends AnyFunSuite with Matchers {
     lowerBounds shouldEqual 3
   }
 
-  test("compound types") {
+  test("compound type") {
     val robot = Robot()
     robot.isRunning shouldBe true
     robot.isEmoting shouldBe true
@@ -78,6 +103,7 @@ class TypesTest extends AnyFunSuite with Matchers {
     type User = String
     type Age = Int
     val users:  Map[User, Age] =  Map("john" -> 21, "jane" -> 19)
+
     users("john") shouldEqual 21
     users("jane") shouldEqual 19
   }
