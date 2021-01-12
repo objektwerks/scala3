eb3dea3640aa0f875a8b951add1af18c67850842
objektwerks <objektwerks@runbox.com>
2021-01-12 09:25:07 -0500

    refactored variance type test

--- src/test/scala/objektwerks/VarianceTypeTest.scala
+++ src/test/scala/objektwerks/VarianceTypeTest.scala
@@ -4,45 +4,45 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 sealed trait Animal
+class Cat extends Animal
 class Dog extends Animal
-
-trait NotNullFilter[-V, +R] {
-  def notNull(value: V): R
-}
-
-object UpperBounds { 
-  def apply[U <: AnyVal](n: U): U = identity(n) 
-}
-object LowerBounds { 
-  def apply[L >: AnyVal](n: L): L = identity(n) 
-}
 
 class VarianceTypeTest extends AnyFunSuite with Matchers {
   test("invariant") {
-    class Vet[T](val animal: T)
-    val dog: Dog = Dog()
-    val vet: Vet[Animal] = Vet[Animal](dog)
-    vet.animal.isInstanceOf[Animal] shouldBe true
-    vet.animal.isInstanceOf[Dog] shouldBe true
+    class Vet[T] {
+      def heal(animal: T): Boolean = true
+    }
+
+    val vet = Vet[Animal]
+    vet.heal( Cat() ) shouldBe true
+    vet.heal( Dog() ) shouldBe true
   }
 
   test("covariant") {
-    class Vet[+T](val animal: T)
-    val dog: Dog = Dog()
-    val vet: Vet[Animal] = Vet[Dog](dog)
-    vet.animal.isInstanceOf[Animal] shouldBe true
-    vet.animal.isInstanceOf[Dog] shouldBe true
+    class Vet[+T] {
+      def heal[S >: T](animal: S): Boolean = true
+    }
+
+    val vet = Vet[Animal]
+    vet.heal( Cat() ) shouldBe true
+    vet.heal( Dog() ) shouldBe true
   }
 
   test("contravariant") {
-    class Vet[-T <: Animal](val animal: Animal)
-    val dog = Dog()
-    val vet: Vet[Dog] = Vet[Animal](dog)
-    vet.animal.isInstanceOf[Animal] shouldBe true
-    vet.animal.isInstanceOf[Dog] shouldBe true
+    class Vet[-T] {
+      def heal(animal: T): Boolean = true
+    }
+
+    val vet = Vet[Animal]
+    vet.heal( Cat() ) shouldBe true
+    vet.heal( Dog() ) shouldBe true
   }
 
   test("contravariant in, covariant out") {
+    trait NotNullFilter[-V, +R] {
+      def notNull(value: V): R
+    }
+
     val filter = new NotNullFilter[String, Boolean] {
       override def notNull(v: String): Boolean = v != null
     }
@@ -51,13 +51,5 @@ class VarianceTypeTest extends AnyFunSuite with Matchers {
     val notNulls = values.filter(v => filter.notNull(v))
 
     notNulls shouldEqual List("a", "b", "c")
-  }
-
-  test("bounds") {
-    val upperBounds: Int = UpperBounds(3)
-    upperBounds shouldEqual 3
-
-    val lowerBounds: Any = LowerBounds(3)
-    lowerBounds shouldEqual 3
   }
 }
