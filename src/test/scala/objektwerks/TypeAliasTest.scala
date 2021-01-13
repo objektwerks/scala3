3331d14dcda89d3c8b655833d8d8b6966f21d2ad
objektwerks <objektwerks@runbox.com>
2021-01-13 08:10:25 -0500

    corrected variance type and type alias tests

--- src/test/scala/objektwerks/TypeAliasTest.scala
+++ src/test/scala/objektwerks/TypeAliasTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class TypeAliasTest extends AnyFunSuite with Matchers {
-    test("type alias") {
+  test("type alias") {
     type User = String
     type Age = Int
     val users =  Map[User, Age]("john" -> 21, "jane" -> 19)
