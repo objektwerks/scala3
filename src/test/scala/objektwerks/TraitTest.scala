4aeedb8d70a5946e8e440ef536276a80868ca726
objektwerks <objektwerks@runbox.com>
2021-01-17 08:44:15 -0500

    transitioned trait test to use python-ruby format

--- src/test/scala/objektwerks/TraitTest.scala
+++ src/test/scala/objektwerks/TraitTest.scala
@@ -3,23 +3,20 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-enum Honorific(honorific: String) {
+enum Honorific(honorific: String):
   case sir extends Honorific("Sir")
   case madam extends Honorific("Madam")
-}
 
-enum Greeting(greeting: String) {
+enum Greeting(greeting: String):
   case goodMorning extends Greeting("I bid you good morning.")
   case goodEvening extends Greeting("I bid you good evening")
-}
 
-transparent trait SalutationBuilder {
+transparent trait SalutationBuilder:
   def build(honorific: Honorific, greeting: Greeting): String = s"$honorific, $greeting"
-}
 
-sealed trait Salutation(val honorific: Honorific, val greeting: Greeting) extends SalutationBuilder {
+
+sealed trait Salutation(val honorific: Honorific, val greeting: Greeting) extends SalutationBuilder:
   def greet(): String = build(honorific, greeting)
-}
 
 class Greeter(honorific: Honorific, greeting: Greeting) extends Salutation(honorific, greeting)
 
