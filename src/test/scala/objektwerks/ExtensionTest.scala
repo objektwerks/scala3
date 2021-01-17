7fefffa74170b1d4d4d3ba344092e6a05a09f554
objektwerks <objektwerks@runbox.com>
2021-01-17 10:13:37 -0500

    transitioned extension test class to python-ruby format

--- src/test/scala/objektwerks/ExtensionTest.scala
+++ src/test/scala/objektwerks/ExtensionTest.scala
@@ -8,9 +8,8 @@ open case class Circle(x: Double, y: Double, radius: Double)
 extension (circle: Circle)
   def circumference: Double = circle.radius * math.Pi * 2
 
-class ExtensionTest extends AnyFunSuite with Matchers {
+class ExtensionTest extends AnyFunSuite with Matchers:
   test("extension") {
     val circle = Circle(3.0, 3.0, 3.0)
     circle.circumference shouldBe 18.84955592153876
   }
-}
