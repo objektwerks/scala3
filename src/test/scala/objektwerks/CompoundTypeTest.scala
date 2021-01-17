483cc8eb2c20ad0c2d2cf4c74946b9f778196202
objektwerks <objektwerks@runbox.com>
2021-01-17 09:31:30 -0500

    transitioned compound type test to use python-ruby format

--- src/test/scala/objektwerks/CompoundTypeTest.scala
+++ src/test/scala/objektwerks/CompoundTypeTest.scala
@@ -3,25 +3,25 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-trait Init { 
+trait Init:
   def init: Boolean = true 
-}
-trait Run extends Init { 
+
+trait Run extends Init:
   def run: Boolean = init
-}
-class Runnable extends Run { 
+
+class Runnable extends Run:
   def isRunning: Boolean = run 
-}
-trait Emotion { 
+
+trait Emotion:
   def isEmoting: Boolean = true 
-}
-trait Speach { 
+
+trait Speach:
   def isSpeaking: Boolean = true 
-}
+
 class Robot extends Runnable with Emotion with Speach
 
 class CompoundTypeTest extends AnyFunSuite with Matchers {
-  test("compound type") {
+  test("compound") {
     val robot = Robot()
     robot.isRunning shouldBe true
     robot.isEmoting shouldBe true
