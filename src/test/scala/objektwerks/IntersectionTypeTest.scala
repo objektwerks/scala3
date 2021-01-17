65336b05bb8b74753149310555271739054d7822
objektwerks <objektwerks@runbox.com>
2021-01-17 08:55:27 -0500

    transitioned intersection test to use python-ruby format; refactored said test

--- src/test/scala/objektwerks/IntersectionTypeTest.scala
+++ src/test/scala/objektwerks/IntersectionTypeTest.scala
@@ -3,20 +3,18 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-sealed trait Camera {
-  def takePhoto(): Boolean = true
-}
+sealed trait Camera:
+  def cameraEnabled(): Boolean = true
 
-sealed trait Phone {
-  def makeCall(): Boolean = true
-}
+sealed trait Phone:
+  def phoneEnabled(): Boolean = true
 
 class MobilePhone extends Camera with Phone
 
-def useMobilePhone(mobilePhone: Camera & Phone): (Boolean, Boolean) = ( mobilePhone.takePhoto(), mobilePhone.makeCall() )
+def verifyMobilePhone(mobilePhone: Camera & Phone): (Boolean, Boolean) = ( mobilePhone.cameraEnabled(), mobilePhone.phoneEnabled() )
 
 class IntersectionTypeTest extends AnyFunSuite with Matchers {
   test("intersection") {
-    useMobilePhone( MobilePhone() ) shouldBe (true, true)
+    verifyMobilePhone( MobilePhone() ) shouldBe (true, true)
   }
 }
