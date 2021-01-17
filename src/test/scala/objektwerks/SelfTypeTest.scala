9ee1306c4c1676dd411d507841e8e888f6c99d25
objektwerks <objektwerks@runbox.com>
2021-01-17 09:58:50 -0500

    transitioned self type test to use python-ruby format

--- src/test/scala/objektwerks/SelfTypeTest.scala
+++ src/test/scala/objektwerks/SelfTypeTest.scala
@@ -3,26 +3,25 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-trait Speaking { 
+trait Speaking:
   def speaking: String 
-}
-trait Hello extends Speaking { 
+
+trait Hello extends Speaking:
   override def speaking = "hello" 
-}
-trait Goodbye extends Speaking { 
+
+trait Goodbye extends Speaking:
   override def speaking = "goodbye" 
-}
-class Speaker {
+
+class Speaker:
   self: Speaking =>
   def speak: String = speaking
-}
 
 class SelfTypeTest extends AnyFunSuite with Matchers {
-    test("self type") {
-    val helloSpeaker = new Speaker() with Hello
+    test("self") {
+    val helloSpeaker = new Speaker with Hello
     helloSpeaker.speak shouldEqual "hello"
 
-    val goodbyeSpeaker = new Speaker() with Goodbye
+    val goodbyeSpeaker = new Speaker with Goodbye
     goodbyeSpeaker.speak shouldEqual "goodbye"
   }
 }
