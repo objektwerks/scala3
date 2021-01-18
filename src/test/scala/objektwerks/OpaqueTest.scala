63ccf03a083b6950bfe466d30308d94a4caaa6f4
objektwerks <objektwerks@runbox.com>
2021-01-18 09:24:57 -0500

    transitioned to python-ruby format: opaque test

--- src/test/scala/objektwerks/OpaqueTest.scala
+++ src/test/scala/objektwerks/OpaqueTest.scala
@@ -3,27 +3,17 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-object Pulses {
-  opaque type Pulse = Int
+opaque type Pulse = Int
 
-  object Pulse {
-    def apply(pulse: Int): Pulse = pulse
-  }
+object Pulse:
+  def apply(pulse: Int): Pulse = pulse
 
-  extension (pulse: Pulse)
-    def asInt: Int = pulse.toInt
-    def asIntOption: Option[Pulse] = if (pulse > 0) Some(pulse) else None
-    def asJson: String = s"""{ "pulse": ${pulse.asInt} }"""
-}
+extension (pulse: Pulse)
+  def asInt: Int = pulse.toInt
 
-class OpaqueTest extends AnyFunSuite with Matchers {
+class OpaqueTest extends AnyFunSuite with Matchers:
   test("opaque") {
-    import Pulses._
-
-    val pulse = Pulse(56)
-    pulse shouldBe Pulse(56)
-    pulse.asInt shouldBe 56
-    pulse.asIntOption shouldBe Some(pulse)
-    pulse.asJson shouldBe s"""{ "pulse": ${pulse.asInt} }"""
+    val pulse = Pulse(21)
+    pulse shouldBe Pulse(21)
+    pulse.asInt shouldBe 21
   }
-}
