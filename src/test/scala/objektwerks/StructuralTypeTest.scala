141838cb5f9b6ed7028fcc5a2a8c0aedc802cce3
objektwerks <objektwerks@runbox.com>
2021-01-18 09:32:39 -0500

    transitioned to python-ruby format: structural type test

--- src/test/scala/objektwerks/StructuralTypeTest.scala
+++ src/test/scala/objektwerks/StructuralTypeTest.scala
@@ -15,14 +15,15 @@ type Auto = Record {
   val year: Int
 }
 
-class StructuralTypeTest extends AnyFunSuite with Matchers {
+class StructuralTypeTest extends AnyFunSuite with Matchers:
   test("structural") {
     val auto = Record("make" -> "Porsche", "model" -> "911", "year" -> 1964).asInstanceOf[Auto]
+
     val make = auto.selectDynamic("make").asInstanceOf[String]
     val model = auto.selectDynamic("model").asInstanceOf[String]
     val year = auto.selectDynamic("year").asInstanceOf[Int]
+    
     make shouldBe "Porsche"
     model shouldBe "911"
     year shouldBe 1964
   }
-}
