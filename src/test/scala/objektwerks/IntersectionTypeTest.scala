f71d01ee5a6f9aa237184e44e6b563f367612777
objektwerks <objektwerks@runbox.com>
2021-01-18 09:03:59 -0500

    refactored intersection type test

--- src/test/scala/objektwerks/IntersectionTypeTest.scala
+++ src/test/scala/objektwerks/IntersectionTypeTest.scala
@@ -4,16 +4,16 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 sealed trait Camera:
-  def cameraEnabled(): Boolean = true
+  def cameraEnabled: Boolean = true
 
 sealed trait Mic:
-  def micEnabled(): Boolean = true
+  def micEnabled: Boolean = true
 
 open class MobilePhone
 
-def verifyFeatures(mobilePhone: Camera & Mic): (Boolean, Boolean) = ( mobilePhone.cameraEnabled(), mobilePhone.micEnabled() )
+def verifyMobilePhone(mobilePhone: Camera & Mic): Boolean = mobilePhone.cameraEnabled && mobilePhone.micEnabled
 
 class IntersectionTypeTest extends AnyFunSuite with Matchers:
   test("intersection") {
-    verifyFeatures( new MobilePhone() with Camera with Mic ) shouldBe (true, true)
+    verifyMobilePhone( new MobilePhone() with Camera with Mic ) shouldBe true
   }
