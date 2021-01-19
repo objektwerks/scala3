858df0337d989d81c5bdafa3dc1997604307b7a9
objektwerks <objektwerks@runbox.com>
2021-01-19 04:42:28 -0500

    refactored compound type test

--- src/test/scala/objektwerks/CompoundTypeTest.scala
+++ src/test/scala/objektwerks/CompoundTypeTest.scala
@@ -6,22 +6,18 @@ import org.scalatest.matchers.should.Matchers
 sealed trait Boot:
   def boot: Boolean = true
 
-sealed trait Walk:
-  def walk: Boolean = true
-
-sealed trait Stop:
-  def stop: Boolean = true 
+sealed trait Greet:
+  def greet(name: String): String = s"Greetings, $name!"
 
 sealed trait Shutdown:
   def shutdown: Boolean = true 
 
-final class Robot extends Boot with Walk with Stop with Shutdown
+final class Robot extends Boot with Greet with Shutdown
 
 class CompoundTypeTest extends AnyFunSuite with Matchers:
   test("compound") {
     val robot = Robot()
     robot.boot shouldBe true
-    robot.walk shouldBe true
-    robot.stop shouldBe true
+    robot.greet("Barney") shouldBe "Greetings, Barney!"
     robot.shutdown shouldBe true
   }
