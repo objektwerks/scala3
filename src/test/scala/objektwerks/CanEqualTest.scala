64c55f6a44916ffcd6465d9c9f285ec2bd75eae0
objektwerks <objektwerks@runbox.com>
2021-01-22 06:53:09 -0500

    added can equal test

---
+++ src/test/scala/objektwerks/CanEqualTest.scala
@@ -0,0 +1,30 @@
+package objektwerks
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+import scala.language.strictEquality
+
+sealed trait Book:
+    def author: String
+    def title: String
+    def year: Int
+
+final case class PrintedBook(author: String, title: String, year: Int, pages: Int) extends Book
+final case class AudioBook(author: String, title: String, year: Int, minutes: Int) extends Book
+
+given CanEqual[PrintedBook, PrintedBook] = CanEqual.derived
+given CanEqual[AudioBook, AudioBook] = CanEqual.derived
+
+class CanEqualTest extends AnyFunSuite with Matchers {
+  test("can equal") {
+    val pb1 = PrintedBook("Beer Bible", "Fred Flintstone", 2000, 425)
+    val pb2 = PrintedBook("Beer Bible", "Fred Flintstone", 2000, 425)
+    val pb3 = PrintedBook("IPA Recipes", "Barney Rebel", 2001, 325)
+    val ab1 = AudioBook("IPA Recipes", "Barney Rebel", 2001, 650)
+
+    (pb1 == pb2) shouldBe true
+    (pb1 == pb3) shouldBe false
+    // Can't compare different book types: (pb3 == ab1) shouldBe false
+  }
+}
