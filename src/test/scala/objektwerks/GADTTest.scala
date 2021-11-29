e1ea4866a0aa53d071efe8ede938198b708281d5
objektwerks <objektwerks@runbox.com>
2021-11-29 08:52:36 -0500

    refactored GADT test

--- src/test/scala/objektwerks/GADTTest.scala
+++ src/test/scala/objektwerks/GADTTest.scala
@@ -3,22 +3,23 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-sealed trait Expression[T]
-final case class IntExpr(int: Int) extends Expression[Int]
-final case class BoolExpr(boolean: Boolean) extends Expression[Boolean]
-
-def eval[T](expression: Expression[T]): T = expression match
-  case IntExpr(int) => int
-  case BoolExpr(boolean) => boolean
-
-enum Box[T](content: T):
-  def unbox: T = content
-
-  case IntBox(number: Int) extends Box[Int](number)
-  case BoolBox(boolean: Boolean) extends Box[Boolean](boolean)
-end Box // end Box is optional!!!  
-
 class GADTTest extends AnyFunSuite with Matchers:
+  sealed trait Expression[T]
+  final case class IntExpr(int: Int) extends Expression[Int]
+  final case class BoolExpr(boolean: Boolean) extends Expression[Boolean]
+
+  def eval[T](expression: Expression[T]): T = expression match
+    case IntExpr(int) => int
+    case BoolExpr(boolean) => boolean
+    case _ => fail()
+
+  enum Box[T](content: T):
+    def unbox: T = content
+
+    case IntBox(number: Int) extends Box[Int](number)
+    case BoolBox(boolean: Boolean) extends Box[Boolean](boolean)
+  end Box // end Box is optional!!!  
+
   test("gadt") {
     eval( IntExpr(3) ) shouldBe 3
     eval( BoolExpr(true) ) shouldBe true
