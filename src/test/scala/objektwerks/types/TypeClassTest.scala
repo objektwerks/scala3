0f0e5633b1e7f4529f25b9d65a1c7ec8c8a88ae7
objektwerks <objektwerks@runbox.com>
2021-11-30 08:37:56 -0500

    refactored due to package move

--- src/test/scala/objektwerks/types/TypeClassTest.scala
+++ src/test/scala/objektwerks/types/TypeClassTest.scala
@@ -1,32 +1,31 @@
 package objektwerks.types
 
-import objektwerks.{Mimicable, Monoid, SemiGroup}
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-sealed trait SemiGroup[T]:
-  extension (a: T) def join (b: T): T
-
-sealed trait Monoid[T] extends SemiGroup[T]:
-  def zero: T
-
-given Monoid[String] with
-  extension (a: String) def join (b: String): String = a.concat(b)
-  def zero: String = ""
-
-given Monoid[Int] with
-  extension (a: Int) def join (b: Int): Int = a + b
-  def zero: Int = 0
-
-def joinAll[T: Monoid](ts: Seq[T]): T = ts.foldLeft( summon[Monoid[T]].zero )( _.join(_) )
-
-trait Mimicable[A]:
-  extension(a: A) def mimic: String
-
-given Mimicable[String] with
-  extension (a: String) def mimic = a
-
 class TypeClassTest extends AnyFunSuite with Matchers:
+  sealed trait SemiGroup[T]:
+    extension (a: T) def join (b: T): T
+
+  sealed trait Monoid[T] extends SemiGroup[T]:
+    def zero: T
+
+  given Monoid[String] with
+    extension (a: String) def join (b: String): String = a.concat(b)
+    def zero: String = ""
+
+  given Monoid[Int] with
+    extension (a: Int) def join (b: Int): Int = a + b
+    def zero: Int = 0
+
+  def joinAll[T: Monoid](ts: Seq[T]): T = ts.foldLeft( summon[Monoid[T]].zero )( _.join(_) )
+
+  trait Mimicable[A]:
+    extension(a: A) def mimic: String
+
+  given Mimicable[String] with
+    extension (a: String) def mimic = a
+
   test("type class") {
     val strings = Seq("Scala3 ", "is a ", "new language!")
     joinAll(strings) shouldBe "Scala3 is a new language!"
