6f02737d65ade3b4137501a139cc16b5cc98875b
objektwerks <objektwerks@runbox.com>
2021-11-30 08:35:51 -0500

    refactored due to package move

--- src/test/scala/objektwerks/given/GivenUsingTest.scala
+++ src/test/scala/objektwerks/given/GivenUsingTest.scala
@@ -1,21 +1,20 @@
 package objektwerks.`given`
 
-import objektwerks.Combiner
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-sealed trait Combiner[T]:
-  def combine(list: List[T]): T
-
-given Combiner[Int] with
-  def combine(list: List[Int]): Int = list.sum
-
-given Combiner[String] with
-  def combine(list: List[String]): String = list.mkString("")
-
-def combineList[T](list: List[T])(using combiner: Combiner[T]): T = combiner.combine(list)
-
 class GivenUsingTest extends AnyFunSuite with Matchers:
+  sealed trait Combiner[T]:
+    def combine(list: List[T]): T
+
+  given Combiner[Int] with
+    def combine(list: List[Int]): Int = list.sum
+
+  given Combiner[String] with
+    def combine(list: List[String]): String = list.mkString("")
+
+  def combineList[T](list: List[T])(using combiner: Combiner[T]): T = combiner.combine(list)
+
   test("given > using") {
     combineList( List(1, 2, 3) ) shouldBe 6
     combineList( List("Scala3 ", "is a ", "new language!") ) shouldBe "Scala3 is a new language!"
