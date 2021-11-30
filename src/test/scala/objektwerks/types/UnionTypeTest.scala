63c0ec007c6686fab1a68ac4fd48650174f84ff6
objektwerks <objektwerks@runbox.com>
2021-11-30 08:38:24 -0500

    refactored due to package move

--- src/test/scala/objektwerks/types/UnionTypeTest.scala
+++ src/test/scala/objektwerks/types/UnionTypeTest.scala
@@ -1,23 +1,22 @@
 package objektwerks.types
 
-import objektwerks.{Beer, Lager, Pilsner}
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-sealed trait Beer:
-  val name: String
-
-final case class Lager(name: String) extends Beer:
-  def prost: Lager = this
-
-final case class Pilsner(name: String) extends Beer:
-  def cheers: Pilsner = this
-
-def drink(beer: Lager | Pilsner): Beer = beer match
-  case lager @ Lager(name) => lager.prost
-  case pilsner @ Pilsner(name) => pilsner.cheers
-
 class UnionTypeTest extends AnyFunSuite with Matchers:
+  sealed trait Beer:
+    val name: String
+
+  final case class Lager(name: String) extends Beer:
+    def prost: Lager = this
+
+  final case class Pilsner(name: String) extends Beer:
+    def cheers: Pilsner = this
+
+  def drink(beer: Lager | Pilsner): Beer = beer match
+    case lager @ Lager(name) => lager.prost
+    case pilsner @ Pilsner(name) => pilsner.cheers
+
   test("union") {
     val lager = Lager("Song of Joy")
     val pilsner = Pilsner("Poetica 2")
