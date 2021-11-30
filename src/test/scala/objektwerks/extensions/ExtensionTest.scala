3bff283ac82bf12cb158b652e624f4e97906ceb8
objektwerks <objektwerks@runbox.com>
2021-11-30 08:35:15 -0500

    refactored due to package move

--- src/test/scala/objektwerks/extensions/ExtensionTest.scala
+++ src/test/scala/objektwerks/extensions/ExtensionTest.scala
@@ -1,15 +1,14 @@
 package objektwerks.extensions
 
-import objektwerks.Circle
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-open case class Circle(x: Double, y: Double, radius: Double)
-
-extension (circle: Circle)
-  def circumference: Double = circle.radius * math.Pi * 2
-
 class ExtensionTest extends AnyFunSuite with Matchers:
+  open case class Circle(x: Double, y: Double, radius: Double)
+
+  extension (circle: Circle)
+    def circumference: Double = circle.radius * math.Pi * 2
+
   test("extension") {
     val circle = Circle(3.0, 3.0, 3.0)
     circle.circumference shouldBe 18.84955592153876
