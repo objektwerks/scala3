88a5d77a0e6dbb63d06be9624014148c5ea0f20f
objektwerks <objektwerks@runbox.com>
2021-11-30 08:37:31 -0500

    refactored due to package move

--- src/test/scala/objektwerks/types/OpaqueTypeTest.scala
+++ src/test/scala/objektwerks/types/OpaqueTypeTest.scala
@@ -1,22 +1,21 @@
 package objektwerks.types
 
-import objektwerks.Pulses
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-object Pulses:
-  opaque type Pulse = Int
-
-  object Pulse:
-    def apply(pulse: Int): Pulse = pulse
-
-  extension (pulse: Pulse)
-    def asInt: Int = pulse
-
-  extension (pulses: List[Pulse])
-    def avg: Double = pulses.sum / pulses.length
-
 class OpaqueTypeTest extends AnyFunSuite with Matchers:
+  object Pulses:
+    opaque type Pulse = Int
+
+    object Pulse:
+      def apply(pulse: Int): Pulse = pulse
+
+    extension (pulse: Pulse)
+      def asInt: Int = pulse
+
+    extension (pulses: List[Pulse])
+      def avg: Double = pulses.sum / pulses.length
+
   test("opaque") {
     import Pulses.*
     
