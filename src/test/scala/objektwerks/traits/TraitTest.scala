f8537fa9f68c53ba6caeb6c0b5bc9e9b6a7a8db7
objektwerks <objektwerks@runbox.com>
2021-11-30 08:36:48 -0500

    refactored due to package move

--- src/test/scala/objektwerks/traits/TraitTest.scala
+++ src/test/scala/objektwerks/traits/TraitTest.scala
@@ -1,26 +1,25 @@
 package objektwerks.traits
 
-import objektwerks.{Greeting, Honorific, Salutation, SalutationBuilder}
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-enum Honorific(honorific: String):
-  case sir extends Honorific("Sir")
-  case madam extends Honorific("Madam")
-
-enum Greeting(greeting: String):
-  case goodMorning extends Greeting("I bid you good morning.")
-  case goodEvening extends Greeting("I bid you good evening")
-
-transparent trait SalutationBuilder:
-  def build(honorific: Honorific, greeting: Greeting): String = s"$honorific, $greeting"
-
-sealed trait Salutation(val honorific: Honorific, val greeting: Greeting) extends SalutationBuilder:
-  def greet(): String = build(honorific, greeting)
-
-final class Greeter(honorific: Honorific, greeting: Greeting) extends Salutation(honorific, greeting)
-
 class TraitTest extends AnyFunSuite with Matchers:
+  enum Honorific(honorific: String):
+    case sir extends Honorific("Sir")
+    case madam extends Honorific("Madam")
+
+  enum Greeting(greeting: String):
+    case goodMorning extends Greeting("I bid you good morning.")
+    case goodEvening extends Greeting("I bid you good evening")
+
+  transparent trait SalutationBuilder:
+    def build(honorific: Honorific, greeting: Greeting): String = s"$honorific, $greeting"
+
+  sealed trait Salutation(val honorific: Honorific, val greeting: Greeting) extends SalutationBuilder:
+    def greet(): String = build(honorific, greeting)
+
+  final class Greeter(honorific: Honorific, greeting: Greeting) extends Salutation(honorific, greeting)
+
   test("trait") {
     import Greeting.*
     import Honorific.*
