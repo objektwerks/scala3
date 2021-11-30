cc0555d2f13e20b544da7285046fd29243ebe33c
objektwerks <objektwerks@runbox.com>
2021-11-30 08:34:35 -0500

    refactored due to package move

--- src/test/scala/objektwerks/enums/EnumTest.scala
+++ src/test/scala/objektwerks/enums/EnumTest.scala
@@ -1,30 +1,29 @@
 package objektwerks.enums
 
-import objektwerks.{Gate, Planet}
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-enum Gate(val state: Int):
-  case closed extends Gate(0)
-  case open extends Gate(1)
-
-enum Planet(val mass: Double, val radius: Double):
-   val gravity = 6.67300E-11
-
-   def surfaceGravity: Double = gravity * mass / (radius * radius)
-   def surfaceWeight(otherMass: Double): Double = otherMass * surfaceGravity
-
-   case Mercury extends Planet(3.303e+23, 2.4397e6)
-   case Venus   extends Planet(4.869e+24, 6.0518e6)
-   case Earth   extends Planet(5.976e+24, 6.37814e6)
-   case Mars    extends Planet(6.421e+23, 3.3972e6)
-   case Jupiter extends Planet(1.9e+27,   7.1492e7)
-   case Saturn  extends Planet(5.688e+26, 6.0268e7)
-   case Uranus  extends Planet(8.686e+25, 2.5559e7)
-   case Neptune extends Planet(1.024e+26, 2.4746e7)
-end Planet // end Planet is optional!!!
-
 class EnumTest extends AnyFunSuite with Matchers:
+  enum Gate(val state: Int):
+    case closed extends Gate(0)
+    case open extends Gate(1)
+
+  enum Planet(val mass: Double, val radius: Double):
+    val gravity = 6.67300E-11
+
+    def surfaceGravity: Double = gravity * mass / (radius * radius)
+    def surfaceWeight(otherMass: Double): Double = otherMass * surfaceGravity
+
+    case Mercury extends Planet(3.303e+23, 2.4397e6)
+    case Venus   extends Planet(4.869e+24, 6.0518e6)
+    case Earth   extends Planet(5.976e+24, 6.37814e6)
+    case Mars    extends Planet(6.421e+23, 3.3972e6)
+    case Jupiter extends Planet(1.9e+27,   7.1492e7)
+    case Saturn  extends Planet(5.688e+26, 6.0268e7)
+    case Uranus  extends Planet(8.686e+25, 2.5559e7)
+    case Neptune extends Planet(1.024e+26, 2.4746e7)
+  end Planet // end Planet is optional!!!
+
   test("enum") {
     val closed = Gate.closed
     closed shouldBe Gate.closed
