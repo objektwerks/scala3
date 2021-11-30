3f9836f7ea6bbce17a1932138b67b914f7366964
objektwerks <objektwerks@runbox.com>
2021-11-30 08:36:08 -0500

    refactored due to package move

--- src/test/scala/objektwerks/infix/InfixTest.scala
+++ src/test/scala/objektwerks/infix/InfixTest.scala
@@ -1,20 +1,19 @@
 package objektwerks.infix
 
-import objektwerks.Amount
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-case class Amount(value: Double) {
-  def +(other: Amount): Amount = Amount(value + other.value)
-  def -(other: Amount): Amount = Amount(value - other.value)
-  infix def add(other: Amount): Amount = this + other
-  infix def subtract(other: Amount): Amount = this - other
-}
-
-extension (amount: Amount)
-  def discount(discount: Double): Amount = amount.copy(value = amount.value - (amount.value * discount))
-
 class InfixTest extends AnyFunSuite with Matchers:
+  case class Amount(value: Double) {
+    def +(other: Amount): Amount = Amount(value + other.value)
+    def -(other: Amount): Amount = Amount(value - other.value)
+    infix def add(other: Amount): Amount = this + other
+    infix def subtract(other: Amount): Amount = this - other
+  }
+
+  extension (amount: Amount)
+    def discount(discount: Double): Amount = amount.copy(value = amount.value - (amount.value * discount))
+
   test("infix") {
     Amount(1.0) + Amount(2.0) shouldBe Amount(3.0)
     Amount(3.0) - Amount(2.0) shouldBe Amount(1.0)
