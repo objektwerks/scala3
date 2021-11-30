fc6c2b5f8833a35830fe33497968d6b1ab78158b
objektwerks <objektwerks@runbox.com>
2021-11-30 08:37:45 -0500

    refactored due to package move

--- src/test/scala/objektwerks/types/SelfTypeTest.scala
+++ src/test/scala/objektwerks/types/SelfTypeTest.scala
@@ -1,23 +1,22 @@
 package objektwerks.types
 
-import objektwerks.{Goodbye, Hello, Speaker, Speaking}
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-sealed trait Speaking:
-  def speaking: String 
-
-sealed trait Hello extends Speaking:
-  override def speaking = "hello" 
-
-sealed trait Goodbye extends Speaking:
-  override def speaking = "goodbye" 
-
-class Speaker:
-  self: Speaking =>
-  def speak: String = speaking
-
 class SelfTypeTest extends AnyFunSuite with Matchers:
+  sealed trait Speaking:
+    def speaking: String 
+
+  sealed trait Hello extends Speaking:
+    override def speaking = "hello" 
+
+  sealed trait Goodbye extends Speaking:
+    override def speaking = "goodbye" 
+
+  class Speaker:
+    self: Speaking =>
+    def speak: String = speaking  
+
   test("self") {
     val helloSpeaker = new Speaker with Hello
     helloSpeaker.speak shouldEqual "hello"
