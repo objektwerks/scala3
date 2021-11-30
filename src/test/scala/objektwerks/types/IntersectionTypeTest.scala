3b41ef3cfbab1449f4626bee2ab6c4921af99131
objektwerks <objektwerks@runbox.com>
2021-11-30 08:37:18 -0500

    refactored due to package move

--- src/test/scala/objektwerks/types/IntersectionTypeTest.scala
+++ src/test/scala/objektwerks/types/IntersectionTypeTest.scala
@@ -1,20 +1,19 @@
 package objektwerks.types
 
-import objektwerks.{Camera, Mic, MobilePhone}
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-sealed trait Camera:
-  def cameraEnabled: Boolean = true
-
-sealed trait Mic:
-  def micEnabled: Boolean = true
-
-open class MobilePhone
-
-def verifyMobilePhone(mobilePhone: Camera & Mic): Boolean = mobilePhone.cameraEnabled && mobilePhone.micEnabled
-
 class IntersectionTypeTest extends AnyFunSuite with Matchers:
+  sealed trait Camera:
+    def cameraEnabled: Boolean = true
+
+  sealed trait Mic:
+    def micEnabled: Boolean = true
+
+  open class MobilePhone
+
+  def verifyMobilePhone(mobilePhone: Camera & Mic): Boolean = mobilePhone.cameraEnabled && mobilePhone.micEnabled
+
   test("intersection") {
     verifyMobilePhone( new MobilePhone with Camera with Mic ) shouldBe true
   }
