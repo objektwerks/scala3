833ce21600657677e0c2280c556936bfabca827e
objektwerks <objektwerks@runbox.com>
2021-12-05 08:47:47 -0500

    added login command

--- src/main/scala/objektwerks/Command.scala
+++ src/main/scala/objektwerks/Command.scala
@@ -4,5 +4,7 @@ sealed trait Command
 
 final case class Register(email: String) extends Command
 
+final case class Login(email: String, pin: String) extends Command
+
 final case class AddAccount(account: Account) extends Command
 final case class UpdateAccount(account: Account) extends Command
