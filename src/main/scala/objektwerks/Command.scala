5145c1cc75357b6c53ff13dcd00d6bc199a5e749
objektwerks <objektwerks@runbox.com>
2021-12-05 08:49:25 -0500

    added reactivate command

--- src/main/scala/objektwerks/Command.scala
+++ src/main/scala/objektwerks/Command.scala
@@ -3,10 +3,10 @@ package objektwerks
 sealed trait Command
 
 final case class Register(email: String) extends Command
-
 final case class Login(email: String, pin: String) extends Command
 
 final case class Deactivate(license: String) extends Command
+final case class Reactivate(license: String) extends Command
 
 final case class AddAccount(account: Account) extends Command
 final case class UpdateAccount(account: Account) extends Command
