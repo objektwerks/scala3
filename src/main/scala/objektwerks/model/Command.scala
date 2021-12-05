b685b5b952c182f61a1127d8b944dfd3042e56be
objektwerks <objektwerks@runbox.com>
2021-12-05 11:19:17 -0500

    added Type[Entity] to EntityList command

--- src/main/scala/objektwerks/model/Command.scala
+++ src/main/scala/objektwerks/model/Command.scala
@@ -1,5 +1,7 @@
 package objektwerks.model
 
+import scala.quoted.Type
+
 sealed trait Command
 
 final case class Register(email: String) extends Command
@@ -8,6 +10,6 @@ final case class Login(email: String, pin: String) extends Command
 final case class Deactivate(license: String) extends Command
 final case class Reactivate(license: String) extends Command
 
-final case class ListEntity(entity: Entity) extends Command
+final case class ListEntity(entity: Type[Entity]) extends Command
 final case class AddEntity(entity: Entity) extends Command
 final case class UpdateEntity() extends Command
