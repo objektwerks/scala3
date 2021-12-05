cec782f4799732096c8bfff2610f311691ade30f
objektwerks <objektwerks@runbox.com>
2021-12-05 11:09:38 -0500

    refactored events

--- src/main/scala/objektwerks/Event.scala
+++ src/main/scala/objektwerks/Event.scala
@@ -8,13 +8,6 @@ final case class LoggedIn(account: Account) extends Event
 final case class Deactivated(account: Account) extends Event
 final case class Reactivated(account: Account) extends Event
 
-final case class AccountAdded(account: Account) extends Event
-final case class AccountUpdate(account: Account) extends Event
-
-final case class PoolsListed(pools: Seq[Pool]) extends Event
-final case class PoolAdded(pool: Pool) extends Event
-final case class PoolUpdated(pool: Pool) extends Event
-
-final case class SurfacesListed() extends Event
-final case class SurfaceAdded(surface: Surface) extends Event
-final case class SurfaceUpdated(surface: Surface) extends Event
+final case class EntityListed(entities: Seq[Entity]) extends Event
+final case class EntityAdded(entity: Entity)
+final case class EntityUpdated() extends Event
