cb0b811c529d85fd4051b18a9134f7d90a34918a
objektwerks <objektwerks@runbox.com>
2021-12-05 08:52:05 -0500

    added reactivated event, corrected deactivated event

--- src/main/scala/objektwerks/Event.scala
+++ src/main/scala/objektwerks/Event.scala
@@ -5,7 +5,8 @@ sealed trait Event
 case object Registering extends Event
 final case class LoggedIn(account: Account) extends Event
 
-final case class Deactivated(license: String) extends Event
+final case class Deactivated(account: Account) extends Event
+final case class Reactivated(account: Account) extends Event
 
 final case class AccountAdded(account: Account) extends Event
 final case class AccountUpdate(account: Account) extends Event
