35df3a773be9b132ef5ade131d608c2819934766
objektwerks <objektwerks@runbox.com>
2021-12-05 08:48:40 -0500

    added deactivate command

--- src/main/scala/objektwerks/Command.scala
+++ src/main/scala/objektwerks/Command.scala
@@ -6,5 +6,7 @@ final case class Register(email: String) extends Command
 
 final case class Login(email: String, pin: String) extends Command
 
+final case class Deactivate(license: String) extends Command
+
 final case class AddAccount(account: Account) extends Command
 final case class UpdateAccount(account: Account) extends Command
