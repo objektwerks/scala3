872a4ed652ee504fbd82054d8ee647be9829ba60
objektwerks <objektwerks@runbox.com>
2021-12-05 08:46:13 -0500

    added register command

--- src/main/scala/objektwerks/Command.scala
+++ src/main/scala/objektwerks/Command.scala
@@ -2,5 +2,7 @@ package objektwerks
 
 sealed trait Command
 
+final case class Register(email: String) extends Command
+
 final case class AddAccount(account: Account) extends Command
 final case class UpdateAccount(account: Account) extends Command
