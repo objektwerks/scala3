647f65a57e0fbc04590bbb8f1f4f473f0f70d611
objektwerks <objektwerks@runbox.com>
2021-12-05 14:46:34 -0500

    refactored list entity command

--- src/main/scala/objektwerks/model/Command.scala
+++ src/main/scala/objektwerks/model/Command.scala
@@ -1,7 +1,5 @@
 package objektwerks.model
 
-import scala.quoted.Type
-
 sealed trait Command
 
 final case class Register(email: String) extends Command
@@ -10,6 +8,6 @@ final case class Login(email: String, pin: String) extends Command
 final case class Deactivate(license: String) extends Command
 final case class Reactivate(license: String) extends Command
 
-final case class ListEntity(entity: Type[Entity]) extends Command
+final case class ListEntity(entity: Entity) extends Command
 final case class AddEntity(entity: Entity) extends Command
 final case class UpdateEntity(enity: Entity) extends Command
