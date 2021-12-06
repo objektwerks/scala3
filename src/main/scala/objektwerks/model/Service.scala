41d20c9f669fe95c431e9eae97f80aecc5dca325
objektwerks <objektwerks@runbox.com>
2021-12-06 04:15:08 -0500

    added either to service methods

--- src/main/scala/objektwerks/model/Service.scala
+++ src/main/scala/objektwerks/model/Service.scala
@@ -3,10 +3,10 @@ package objektwerks.model
 import scala.concurrent.Future
 
 trait Service:
-  def register(register: Register): Future[Registering]
-  def login(login: Login): Future[LoggedIn]
-  def deactivate(deactivate: Deactivate): Future[Deactivated]
-  def reactivate(reactivate: Reactivate): Future[Reactivated]
-  def listEntity(entity: Entity): Future[EntityListed]
-  def addEntity(entity: Entity): Future[EntityAdded]
-  def updateEntity(entity: Entity): Future[EntityUpdated]
+  def register(register: Register): Future[Either[Fault, Registering]]
+  def login(login: Login): Future[Either[Fault, LoggedIn]]
+  def deactivate(deactivate: Deactivate): Future[Either[Fault, Deactivated]]
+  def reactivate(reactivate: Reactivate): Future[Either[Fault, Reactivated]]
+  def listEntity(entity: Entity): Future[Either[Fault, EntityListed]]
+  def addEntity(entity: Entity): Future[Either[Fault, EntityAdded]]
+  def updateEntity(entity: Entity): Future[Either[Fault, EntityUpdated]]
