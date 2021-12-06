37be9bbce0d48bf358045610af9af1153c66cf1b
objektwerks <objektwerks@runbox.com>
2021-12-06 07:35:35 -0500

    refactored Event

--- src/main/scala/objektwerks/model/Event.scala
+++ src/main/scala/objektwerks/model/Event.scala
@@ -8,6 +8,6 @@ final case class LoggedIn(account: Account) extends Event
 final case class Deactivated(account: Account) extends Event
 final case class Reactivated(account: Account) extends Event
 
-final case class EntityListed(entities: Seq[Entity]) extends Event
-final case class EntityAdded(entity: Entity) extends Event
-final case class EntityUpdated(entity: Entity) extends Event
+final case class Listed(entities: Seq[Entity]) extends Event
+final case class Added(entity: Entity) extends Event
+final case class Updated(entity: Entity) extends Event
