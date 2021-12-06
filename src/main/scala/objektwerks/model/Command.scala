a41f2735eccefdcd1f074934ba832f7a80f4fe8d
objektwerks <objektwerks@runbox.com>
2021-12-06 07:35:56 -0500

    refactored Command

--- src/main/scala/objektwerks/model/Command.scala
+++ src/main/scala/objektwerks/model/Command.scala
@@ -8,6 +8,6 @@ final case class Login(email: String, pin: String) extends Command
 final case class Deactivate(license: String) extends Command
 final case class Reactivate(license: String) extends Command
 
-final case class ListEntity(entity: Entity) extends Command
-final case class AddEntity(entity: Entity) extends Command
-final case class UpdateEntity(enity: Entity) extends Command
+final case class ListPools() extends Command
+final case class AddPool(pool: Pool) extends Command
+final case class UpdatePool(pool: Pool) extends Command
