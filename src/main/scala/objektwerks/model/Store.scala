d685c79d2af4b6f39129addef792ee780f60baf7
objektwerks <objektwerks@runbox.com>
2021-12-07 07:38:18 -0500

    added surface impl for store

--- src/main/scala/objektwerks/model/Store.scala
+++ src/main/scala/objektwerks/model/Store.scala
@@ -5,7 +5,8 @@ import scala.collection.mutable
 object Store:
   private val accounts = mutable.Map.empty[String, Account]
   private val pools = mutable.Map.empty[Int, Pool]
-
+  private val surfaces = mutable.Map.empty[Int, Surface]
+  
   def register(email: String): Account =
     val account = Account(email)
     accounts.addOne(account.license, account).getOrElse(account.license, Account())
@@ -23,6 +24,14 @@ object Store:
 
   def addPool(pool: Pool): Pool =
     val newPool = pool.copy(id = pools.size + 1)
-    pools.addOne(newPool.id, pool).getOrElse(pool.id, Pool())
+    pools.addOne(newPool.id, newPool).getOrElse(newPool.id, Pool())
 
   def updatePool(pool: Pool): Unit = pools.update(pool.id, pool)
+
+  def listSurfaces(): Seq[Surface] = surfaces.values.to(Seq)
+
+  def addSurface(surface: Surface): Surface =
+    val newSurface = surface.copy(id = surfaces.size + 1)
+    surfaces.addOne(newSurface.id, newSurface).getOrElse(newSurface.id, Surface())
+
+  def updateSurface(surface: Surface): Unit = surfaces.update(surface.id, surface)
