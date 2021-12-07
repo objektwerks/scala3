c76ac5e5ad04f6d4249792f1fbfc3cf68b84c8db
objektwerks <objektwerks@runbox.com>
2021-12-07 15:01:00 -0500

    refactored Fault

--- src/main/scala/objektwerks/model/Event.scala
+++ src/main/scala/objektwerks/model/Event.scala
@@ -2,7 +2,7 @@ package objektwerks.model
 
 sealed trait Event
 
-final case class Registering() extends Event
+final case class Registered(account: Account) extends Event
 final case class LoggedIn(account: Account) extends Event
 
 final case class Deactivated(account: Account) extends Event
@@ -12,22 +12,11 @@ final case class Listed(entities: Seq[Entity]) extends Event
 final case class Added(entity: Entity) extends Event
 final case class Updated() extends Event
 
-final case class Fault(dateOf: Int,
-                       timeOf: Int,
-                       nanoOf: Int,
-                       code: Int,
+final case class Fault(dateOf: Int = DateTime.currentDate,
+                       timeOf: Int = DateTime.currentTime,
+                       nanoOf: Int = DateTime.nano,
                        cause: String) extends Event
 
 object Fault {
-  val serviceFailure = 500
-
-  def apply(code: Int, cause: String): Fault = Fault(
-    dateOf = DateTime.currentDate,
-    timeOf = DateTime.currentTime,
-    nanoOf = DateTime.nano,
-    code = code,
-    cause = cause
-  )
-
-  def apply(cause: String): Fault = apply(serviceFailure, cause)
+  def apply(throwable: Throwable): Fault = Fault(cause = throwable.getMessage)
 }
