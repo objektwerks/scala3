4345f1efd6925ed619a78bcb00ea9cd655f0fd77
objektwerks <objektwerks@runbox.com>
2021-12-07 11:53:42 -0500

    refactored all service update methods to return unit

--- src/main/scala/objektwerks/model/Service.scala
+++ src/main/scala/objektwerks/model/Service.scala
@@ -12,48 +12,48 @@ trait Service:
 
   def listPools(): Either[Throwable, Seq[Pool]]
   def addPool(pool: Pool): Either[Throwable, Pool]
-  def updatePool(pool: Pool): Either[Throwable, Pool]
+  def updatePool(pool: Pool): Either[Throwable, Unit]
 
   def listSurfaces(poolId: Int): Either[Throwable, Seq[Surface]]
   def addSurface(surface: Surface): Either[Throwable, Surface]
-  def updateSurface(surface: Surface): Either[Throwable, Surface]
+  def updateSurface(surface: Surface): Either[Throwable, Unit]
 
   def listPumps(poolId: Int): Either[Throwable, Seq[Pump]]
   def addPump(pump: Pump): Either[Throwable, Pump]
-  def updatePump(pump: Pump): Either[Throwable, Pump]
+  def updatePump(pump: Pump): Either[Throwable, Unit]
 
   def listTimers(poolId: Int): Either[Throwable, Seq[Timer]]
   def addTimer(timer: Timer): Either[Throwable, Timer]
-  def updateTimer(timer: Timer): Either[Throwable, Timer]
+  def updateTimer(timer: Timer): Either[Throwable, Unit]
 
   def listTimerSettings(timerId: Int): Either[Throwable, Seq[TimerSetting]]
   def addTimerSetting(timerSetting: TimerSetting): Either[Throwable, TimerSetting]
-  def updateTimerSetting(timerSetting: TimerSetting): Either[Throwable, TimerSetting]
+  def updateTimerSetting(timerSetting: TimerSetting): Either[Throwable, Unit]
 
   def listHeaters(poolId: Int): Either[Throwable, Seq[Heater]]
   def addHeater(heater: Heater): Either[Throwable, Heater]
-  def updateHeater(heater: Heater): Either[Throwable, Heater]
+  def updateHeater(heater: Heater): Either[Throwable, Unit]
 
   def listHeaterSettings(heaterId: Int): Either[Throwable, Seq[HeaterSetting]]
   def addHeaterSetting(heaterSetting: HeaterSetting): Either[Throwable, HeaterSetting]
-  def updateHeaterSetting(heaterSetting: HeaterSetting): Either[Throwable, HeaterSetting]
+  def updateHeaterSetting(heaterSetting: HeaterSetting): Either[Throwable, Unit]
 
   def listMeasurements(poolId: Int): Either[Throwable, Seq[Measurement]]
   def addMeasurement(measurement: Measurement): Either[Throwable, Measurement]
-  def updateMeasurement(measurement: Measurement): Either[Throwable, Measurement]
+  def updateMeasurement(measurement: Measurement): Either[Throwable, Unit]
 
   def listCleanings(poolId: Int): Either[Throwable, Seq[Cleaning]]
   def addCleaning(cleaning: Cleaning): Either[Throwable, Cleaning]
-  def updateCleaning(cleaning: Cleaning): Either[Throwable, Cleaning]
+  def updateCleaning(cleaning: Cleaning): Either[Throwable, Unit]
 
   def listChemicals(poolId: Int): Either[Throwable, Seq[Chemical]]
   def addChemical(chemical: Chemical): Either[Throwable, Chemical]
-  def updateChemical(chemical: Chemical): Either[Throwable, Chemical]
+  def updateChemical(chemical: Chemical): Either[Throwable, Unit]
 
   def listSupplies(poolId: Int): Either[Throwable, Seq[Supply]]
   def addSupply(supply: Supply): Either[Throwable, Supply]
-  def updateSupply(supply: Supply): Either[Throwable, Supply]
+  def updateSupply(supply: Supply): Either[Throwable, Unit]
 
   def listRepairs(poolId: Int): Either[Throwable, Seq[Repair]]
   def addRepair(repair: Repair): Either[Throwable, Repair]
-  def updateRepair(repair: Repair): Either[Throwable, Repair]
+  def updateRepair(repair: Repair): Either[Throwable, Unit]
