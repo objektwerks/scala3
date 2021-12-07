d44b7571e4893f288ba9afe67c1ea05cecb4e6d1
objektwerks <objektwerks@runbox.com>
2021-12-07 07:22:35 -0500

    added Fault to Event

--- src/main/scala/objektwerks/model/Event.scala
+++ src/main/scala/objektwerks/model/Event.scala
@@ -11,3 +11,23 @@ final case class Reactivated(account: Account) extends Event
 final case class Listed(entities: Seq[Entity]) extends Event
 final case class Added(entity: Entity) extends Event
 final case class Updated(entity: Entity) extends Event
+
+final case class Fault(dateOf: Int,
+                       timeOf: Int,
+                       nanoOf: Int,
+                       code: Int,
+                       cause: String) extends Event
+
+object Fault {
+  val serviceFailure = 500
+
+  def apply(code: Int, cause: String): Fault = Fault(
+    dateOf = DateTime.currentDate,
+    timeOf = DateTime.currentTime,
+    nanoOf = DateTime.nano,
+    code = code,
+    cause = cause
+  )
+
+  def apply(cause: String): Fault = apply(serviceFailure, cause)
+}
