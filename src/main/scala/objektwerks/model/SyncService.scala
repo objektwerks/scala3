5c5723984f6cfc36578089d670e7c67cd20d5e95
objektwerks <objektwerks@runbox.com>
2021-12-08 08:47:10 -0500

    wrapped sync service calls in Try(...).toEither

--- src/main/scala/objektwerks/model/SyncService.scala
+++ src/main/scala/objektwerks/model/SyncService.scala
@@ -1,9 +1,11 @@
 package objektwerks.model
 
+import scala.util.Try
+
 import Validator._
 
 class SyncService(store: Store) extends Service:
-  def register(email: String): Either[Throwable, Account] = Right( store.register(email) )
+  def register(email: String): Either[Throwable, Account] = Try( store.register(email) ).toEither
 
   def login(email: String, pin: String): Either[Throwable, Account] =
     store.login(email, pin) match
@@ -24,50 +26,50 @@ class SyncService(store: Store) extends Service:
       case Some(account) => Right(account)
       case None => Left(IllegalArgumentException(s"License is invalid: $license"))
 
-  def listPools(): Either[Throwable, Seq[Pool]] = Right( store.listPools() )
-  def addPool(pool: Pool): Either[Throwable, Pool] = Right( store.addPool(pool) )
-  def updatePool(pool: Pool): Either[Throwable, Unit] = Right( store.updatePool(pool) )
+  def listPools(): Either[Throwable, Seq[Pool]] = Try( store.listPools() ).toEither
+  def addPool(pool: Pool): Either[Throwable, Pool] = Try( store.addPool(pool) ).toEither
+  def updatePool(pool: Pool): Either[Throwable, Unit] = Try( store.updatePool(pool) ).toEither
 
-  def listSurfaces(poolId: Int): Either[Throwable, Seq[Surface]] = Right( store.listSurfaces() )
-  def addSurface(surface: Surface): Either[Throwable, Surface] = Right( store.addSurface(surface) )
-  def updateSurface(surface: Surface): Either[Throwable, Unit] = Right( store.updateSurface(surface) )
+  def listSurfaces(poolId: Int): Either[Throwable, Seq[Surface]] = Try( store.listSurfaces() ).toEither
+  def addSurface(surface: Surface): Either[Throwable, Surface] = Try( store.addSurface(surface) ).toEither
+  def updateSurface(surface: Surface): Either[Throwable, Unit] = Try( store.updateSurface(surface) ).toEither
 
-  def listPumps(poolId: Int): Either[Throwable, Seq[Pump]] = Right( store.listPumps() )
-  def addPump(pump: Pump): Either[Throwable, Pump] = Right( store.addPump(pump) )
-  def updatePump(pump: Pump): Either[Throwable, Unit] = Right( store.updatePump(pump) )
+  def listPumps(poolId: Int): Either[Throwable, Seq[Pump]] = Try( store.listPumps() ).toEither
+  def addPump(pump: Pump): Either[Throwable, Pump] = Try( store.addPump(pump) ).toEither
+  def updatePump(pump: Pump): Either[Throwable, Unit] = Try( store.updatePump(pump) ).toEither
 
-  def listTimers(poolId: Int): Either[Throwable, Seq[Timer]] = Right( store.listTiimers() )
-  def addTimer(timer: Timer): Either[Throwable, Timer] = Right( store.addTimer(timer) )
-  def updateTimer(timer: Timer): Either[Throwable, Unit] = Right( store.updateTimer(timer) )
+  def listTimers(poolId: Int): Either[Throwable, Seq[Timer]] = Try( store.listTiimers() ).toEither
+  def addTimer(timer: Timer): Either[Throwable, Timer] = Try( store.addTimer(timer) ).toEither
+  def updateTimer(timer: Timer): Either[Throwable, Unit] = Try( store.updateTimer(timer) ).toEither
 
-  def listTimerSettings(timerId: Int): Either[Throwable, Seq[TimerSetting]] = Right( store.listTiimerSettings() )
-  def addTimerSetting(timerSetting: TimerSetting): Either[Throwable, TimerSetting] = Right( store.addTimerSetting(timerSetting) )
-  def updateTimerSetting(timerSetting: TimerSetting): Either[Throwable, Unit] = Right( store.updateTimerSetting(timerSetting) )
+  def listTimerSettings(timerId: Int): Either[Throwable, Seq[TimerSetting]] = Try( store.listTiimerSettings() ).toEither
+  def addTimerSetting(timerSetting: TimerSetting): Either[Throwable, TimerSetting] = Try( store.addTimerSetting(timerSetting) ).toEither
+  def updateTimerSetting(timerSetting: TimerSetting): Either[Throwable, Unit] = Try( store.updateTimerSetting(timerSetting) ).toEither
 
-  def listHeaters(poolId: Int): Either[Throwable, Seq[Heater]] = Right( store.listHeaters() )
-  def addHeater(heater: Heater): Either[Throwable, Heater] = Right( store.addHeater(heater) )
-  def updateHeater(heater: Heater): Either[Throwable, Unit] = Right( store.updateHeater(heater) )
+  def listHeaters(poolId: Int): Either[Throwable, Seq[Heater]] = Try( store.listHeaters() ).toEither
+  def addHeater(heater: Heater): Either[Throwable, Heater] = Try( store.addHeater(heater) ).toEither
+  def updateHeater(heater: Heater): Either[Throwable, Unit] = Try( store.updateHeater(heater) ).toEither
 
-  def listHeaterSettings(heaterId: Int): Either[Throwable, Seq[HeaterSetting]] = Right( store.listHeaterSettings() )
-  def addHeaterSetting(heaterSetting: HeaterSetting): Either[Throwable, HeaterSetting] = Right( store.addHeaterSetting(heaterSetting) )
-  def updateHeaterSetting(heaterSetting: HeaterSetting): Either[Throwable, Unit] = Right( store.updateHeaterSetting(heaterSetting) )
+  def listHeaterSettings(heaterId: Int): Either[Throwable, Seq[HeaterSetting]] = Try( store.listHeaterSettings() ).toEither
+  def addHeaterSetting(heaterSetting: HeaterSetting): Either[Throwable, HeaterSetting] = Try( store.addHeaterSetting(heaterSetting) ).toEither
+  def updateHeaterSetting(heaterSetting: HeaterSetting): Either[Throwable, Unit] = Try( store.updateHeaterSetting(heaterSetting) ).toEither
 
-  def listMeasurements(poolId: Int): Either[Throwable, Seq[Measurement]] = Right( store.listMeasurements() )
-  def addMeasurement(measurement: Measurement): Either[Throwable, Measurement] = Right( store.addMeasurement(measurement) )
-  def updateMeasurement(measurement: Measurement): Either[Throwable, Unit] = Right( store.updateMeasurement(measurement) )
+  def listMeasurements(poolId: Int): Either[Throwable, Seq[Measurement]] = Try( store.listMeasurements() ).toEither
+  def addMeasurement(measurement: Measurement): Either[Throwable, Measurement] = Try( store.addMeasurement(measurement) ).toEither
+  def updateMeasurement(measurement: Measurement): Either[Throwable, Unit] = Try( store.updateMeasurement(measurement) ).toEither
 
-  def listCleanings(poolId: Int): Either[Throwable, Seq[Cleaning]] = Right( store.listCleanings() )
-  def addCleaning(cleaning: Cleaning): Either[Throwable, Cleaning] = Right( store.addCleaning(cleaning) )
-  def updateCleaning(cleaning: Cleaning): Either[Throwable, Unit] = Right( store.updateCleaning(cleaning) )
+  def listCleanings(poolId: Int): Either[Throwable, Seq[Cleaning]] = Try( store.listCleanings() ).toEither
+  def addCleaning(cleaning: Cleaning): Either[Throwable, Cleaning] = Try( store.addCleaning(cleaning) ).toEither
+  def updateCleaning(cleaning: Cleaning): Either[Throwable, Unit] = Try( store.updateCleaning(cleaning) ).toEither
 
-  def listChemicals(poolId: Int): Either[Throwable, Seq[Chemical]] = Right( store.listChemicals() )
-  def addChemical(chemical: Chemical): Either[Throwable, Chemical] = Right( store.addChemical(chemical) )
-  def updateChemical(chemical: Chemical): Either[Throwable, Unit] = Right( store.updateChemical(chemical) )
+  def listChemicals(poolId: Int): Either[Throwable, Seq[Chemical]] = Try( store.listChemicals() ).toEither
+  def addChemical(chemical: Chemical): Either[Throwable, Chemical] = Try( store.addChemical(chemical) ).toEither
+  def updateChemical(chemical: Chemical): Either[Throwable, Unit] = Try( store.updateChemical(chemical) ).toEither
 
-  def listSupplies(poolId: Int): Either[Throwable, Seq[Supply]] = Right( store.listSupplies() )
-  def addSupply(supply: Supply): Either[Throwable, Supply] = Right( store.addSupply(supply) )
-  def updateSupply(supply: Supply): Either[Throwable, Unit] = Right( store.updateSupply(supply) )
+  def listSupplies(poolId: Int): Either[Throwable, Seq[Supply]] = Try( store.listSupplies() ).toEither
+  def addSupply(supply: Supply): Either[Throwable, Supply] = Try( store.addSupply(supply) ).toEither
+  def updateSupply(supply: Supply): Either[Throwable, Unit] = Try( store.updateSupply(supply) ).toEither
 
-  def listRepairs(poolId: Int): Either[Throwable, Seq[Repair]] = Right( store.listRepairs() )
-  def addRepair(repair: Repair): Either[Throwable, Repair] = Right( store.addRepair(repair) )
-  def updateRepair(repair: Repair): Either[Throwable, Unit] = Right( store.updateRepair(repair) )
+  def listRepairs(poolId: Int): Either[Throwable, Seq[Repair]] = Try( store.listRepairs() ).toEither
+  def addRepair(repair: Repair): Either[Throwable, Repair] = Try( store.addRepair(repair) ).toEither
+  def updateRepair(repair: Repair): Either[Throwable, Unit] = Try( store.updateRepair(repair) ).toEither
