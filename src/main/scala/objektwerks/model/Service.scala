5fb61c923f52b1f8b0e78dfd548cce73e5ca9d58
objektwerks <objektwerks@runbox.com>
2021-12-07 20:24:33 -0500

    edited service

--- src/main/scala/objektwerks/model/Service.scala
+++ src/main/scala/objektwerks/model/Service.scala
@@ -1,8 +1,5 @@
 package objektwerks.model
 
-object Service:
-  def syncService: SyncService = SyncService()
-
 trait Service:
   def register(email: String): Either[Throwable, Account]
   def login(email: String, pin: String): Either[Throwable, Account]
