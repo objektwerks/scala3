0fbdcd89ac0ab099c11c294b21dc579c5ce18dec
objektwerks <objektwerks@runbox.com>
2021-12-08 16:02:21 -0500

    renamed sync service to service

--- src/main/scala/objektwerks/model/Service.scala
+++ src/main/scala/objektwerks/model/Service.scala
@@ -2,7 +2,7 @@ package objektwerks.model
 
 import scala.util.Try
 
-class SyncService(store: Store):
+class Service(store: Store):
   def register(email: String): Either[Throwable, Account] =
     store.register(email) match
       case Some(account) => Right(account)
