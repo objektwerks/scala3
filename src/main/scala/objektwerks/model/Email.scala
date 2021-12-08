09f7a710cd50412561a02c52f356b8351c9d3d04
objektwerks <objektwerks@runbox.com>
2021-12-08 12:06:14 -0500

    refactored email

--- src/main/scala/objektwerks/model/Email.scala
+++ src/main/scala/objektwerks/model/Email.scala
@@ -3,5 +3,6 @@ package objektwerks.model
 final case class Email(id : String,
                        license: String,
                        address: String,
+                       message: String,
                        processed: Boolean = false,
                        valid: Boolean = false)
