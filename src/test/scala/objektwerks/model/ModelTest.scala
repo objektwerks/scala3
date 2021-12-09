7f456213b126be7ddc5fd3f07e09dfee6d4249ad
objektwerks <objektwerks@runbox.com>
2021-12-09 15:00:25 -0500

    refactored model test

--- src/test/scala/objektwerks/model/ModelTest.scala
+++ src/test/scala/objektwerks/model/ModelTest.scala
@@ -9,80 +9,90 @@ class ModelTest extends AnyFunSuite with Matchers:
   val store = Store()
   val service = Service(store)
   val dispatcher = Dispatcher(service)
-  var account = Account(email = "test@test.com")
-  var pool = Pool(license = account.license, name = "test", built = DateTime.localDateToInt(2001, 10, 15))
-  var surface = Surface(poolId = pool.id, installed = DateTime.localDateToInt(2001, 10, 15), kind = "concrete")
 
   test("model") {
-    testRegister()
-    testLogin()
-    testDeactivate()
-    testReactivate()
-    testPool()
-    testSurface()
+    var account = testRegister()
+    testLogin(account)
+    account = testDeactivate(account)
+    account = testReactivate(account)
+
+    var pool = Pool(license = account.license, name = "test", built = DateTime.localDateToInt(2001, 10, 15))
+    pool = testAddPool(pool)
+    testListPools(account)
+    pool = testUpdatePool(pool)
+
+    var surface = Surface(poolId = pool.id, installed = DateTime.localDateToInt(2001, 10, 15), kind = "concrete")
+    surface = testAddSurface(pool, surface)
+    testListSurfaces(pool)
+    surface = testUpdateSurface(pool, surface)
   }
 
-  def testRegister(): Unit =
+  def testRegister(): Account =
     val command = Register(email = "test@test.com")
     dispatcher.dispatch(command) match
       case Registered(account) =>
         account.isActivated shouldBe true
-        this.account = account
+        account
       case _ => fail()
 
-  def testLogin(): Unit =
+  def testLogin(account: Account): Unit =
     val command = Login(account.email, account.pin)
     dispatcher.dispatch(command) match
-      case LoggedIn(account) =>
-        this.account shouldBe account
-        this.account = account
+      case event: LoggedIn => account shouldBe event.account
       case _ => fail()
 
-  def testDeactivate(): Unit =
+  def testDeactivate(account: Account): Account =
     val command = Deactivate(account.license)
     dispatcher.dispatch(command) match
       case Deactivated(account) =>
         account.isDeactivated shouldBe true
-        this.account = account
+        account
       case _ => fail()
 
-  def testReactivate(): Unit =
+  def testReactivate(account: Account): Account =
     val command = Reactivate(account.license)
     dispatcher.dispatch(command) match
       case Reactivated(account) =>
         account.isActivated shouldBe true
-        this.account = account
+        account
       case _ => fail()
 
-  def testPool(): Unit =
-    val add = AddPool(account.license, pool)
+  def testAddPool(pool: Pool): Pool =
+    val add = AddPool(pool.license, pool)
     dispatcher.dispatch(add) match
       case Added(pool: Pool) =>
         pool.id > 0 shouldBe true
-        this.pool = pool
+        pool
       case _ => fail()
 
+  def testListPools(account: Account): Unit =
     val list = ListPools(account.license)
     dispatcher.dispatch(list) match
       case Listed(pools) => pools.size shouldBe 1
       case _ => fail()
 
-    val update = UpdatePool(account.license, pool.copy(volume = 10000))
+  def testUpdatePool(pool: Pool): Pool =
+    val updatedPool = pool.copy(volume = 10000)
+    val update = UpdatePool(pool.license, updatedPool)
     dispatcher.dispatch(update) shouldBe Updated()
+    updatedPool
 
-  def testSurface(): Unit =
-    val add = AddSurface(account.license, surface)
+  def testAddSurface(pool: Pool, surface: Surface): Surface =
+    val add = AddSurface(pool.license, surface)
     dispatcher.dispatch(add) match
       case Added(surface: Surface) =>
         surface.id > 0 shouldBe true
-        this.surface = surface
-      case fault: Fault => println(fault)
+        surface
       case _ => fail()
 
-    val list = ListSurfaces(account.license, pool.id)
+  def testListSurfaces(pool: Pool): Unit =
+    val list = ListSurfaces(pool.license, pool.id)
     dispatcher.dispatch(list) match
       case Listed(surfaces) => surfaces.size shouldBe 1
       case _ => fail()
 
-    val update = UpdateSurface(account.license, surface.copy(kind = "pebble"))
+  def testUpdateSurface(pool: Pool, surface: Surface): Surface =
+    val updatedSurface = surface.copy(kind = "pebble")
+    val update = UpdateSurface(pool.license, updatedSurface)
     dispatcher.dispatch(update) shouldBe Updated()
+    updatedSurface
