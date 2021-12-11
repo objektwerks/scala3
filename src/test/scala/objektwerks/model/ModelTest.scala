eab06188fdf1ce22419c148bb62267b533aeee70
objektwerks <objektwerks@runbox.com>
2021-12-11 17:22:34 -0500

    refactored UoM access in model test

--- src/test/scala/objektwerks/model/ModelTest.scala
+++ src/test/scala/objektwerks/model/ModelTest.scala
@@ -4,6 +4,7 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 import Validator._
+import UoM._
 
 class ModelTest extends AnyFunSuite with Matchers:
   val store = Store()
@@ -61,12 +62,12 @@ class ModelTest extends AnyFunSuite with Matchers:
     testListCleanings(pool)
     testUpdateCleaning(pool, cleaning.copy(deck = true))
 
-    var chemical = Chemical(poolId = pool.id, added = 20010101, chemical = "chlorine", amount = 1.0, unit = UoM.gallon.abrv)
+    var chemical = Chemical(poolId = pool.id, added = 20010101, chemical = "chlorine", amount = 1.0, unit = gallon.abrv)
     chemical = testAddChemical(pool, chemical)
     testListChemicals(pool)
     testUpdateChemical(pool, chemical.copy(amount = 2.0))
 
-    var supply = Supply(poolId = pool.id, purchased = 20010101, item = "chlorine", amount = 1.0, unit = UoM.gallon.abrv, cost = 5.00)
+    var supply = Supply(poolId = pool.id, purchased = 20010101, item = "chlorine", amount = 1.0, unit = gallon.abrv, cost = 5.00)
     supply = testAddSupply(pool, supply)
     testListSupplies(pool)
     testUpdateSupply(pool, supply.copy(cost = 6.0))
