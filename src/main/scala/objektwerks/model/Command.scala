9f88228dfb2a8c379173b99f9ba89259862da56b
objektwerks <objektwerks@runbox.com>
2021-12-15 07:32:24 -0500

    refactored authorizor

--- src/main/scala/objektwerks/model/Command.scala
+++ src/main/scala/objektwerks/model/Command.scala
@@ -3,12 +3,10 @@ package objektwerks.model
 sealed trait License:
   val license: String
 
-sealed trait NoLicense
-
 sealed trait Command
 
-final case class Register(email: String) extends Command with NoLicense
-final case class Login(email: String, pin: String) extends Command with NoLicense
+final case class Register(email: String) extends Command
+final case class Login(email: String, pin: String) extends Command
 
 final case class Deactivate(license: String) extends Command with License
 final case class Reactivate(license: String) extends Command with License
