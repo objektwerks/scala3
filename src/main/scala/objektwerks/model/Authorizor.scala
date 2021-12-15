e0d1b0a9bfaa9d6915367581118324b07b1d4d94
objektwerks <objektwerks@runbox.com>
2021-12-15 06:56:42 -0500

    refactored authorizor

--- src/main/scala/objektwerks/model/Authorizor.scala
+++ src/main/scala/objektwerks/model/Authorizor.scala
@@ -4,4 +4,4 @@ class Authorizor(service: Service):
   def authorize(command: Command): Event =
     command match
       case license: License => service.authorize(license.license)
-      case _: NoLicense => Authorized()
+      case _: NoLicense => AuthorizationNotRequired()
