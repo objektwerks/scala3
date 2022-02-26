e3981d051996d89d33f40930b3edf8ea04f99ed8
objektwerks <objektwerks@runbox.com>
2022-02-26 07:49:50 -0500

    added tests to opaque test

--- src/test/scala/objektwerks/types/OpaqueTypeTest.scala
+++ src/test/scala/objektwerks/types/OpaqueTypeTest.scala
@@ -35,6 +35,20 @@ extension (mac: MAC)
   def number: Long = java.lang.Long.parseLong(address, 16)
 
 class OpaqueTypeTest extends AnyFunSuite with Matchers:
-  test("mac") {
-    
+  private val address = "50ed3c45f4ba"
+
+  test("valid mac") {
+    MAC(address).foreach(a => a shouldBe address)
+  }
+
+  test("invalid mac") {
+    MAC("1a2b3c").foreach(a => a shouldBe "")
+  }
+
+  test("display mac") {
+    MAC(address).foreach(a => a.display shouldBe "50:ed:3c:45:f4:ba")
+  }
+
+  test("number mac") {
+    MAC(address).foreach(a => a.number shouldBe 88979848688826L)
   }
