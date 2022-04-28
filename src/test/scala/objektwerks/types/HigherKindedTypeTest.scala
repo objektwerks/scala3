95bbcd3d106b9a969f5b47e6651dabbf3dc0ed03
objektwerks <objektwerks@runbox.com>
2022-04-28 13:33:12 -0400

    reformatted functor

--- src/test/scala/objektwerks/types/HigherKindedTypeTest.scala
+++ src/test/scala/objektwerks/types/HigherKindedTypeTest.scala
@@ -4,9 +4,8 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class HigherKindedTypeTest extends AnyFunSuite with Matchers:
-  trait Functor[F[_]] {
+  trait Functor[F[_]]:
     def map[A, B](fa: F[A])(f: A => B): F[B]
-  }
 
   val listFunctor = new Functor[List] {
     override def map[A, B](xs: List[A])(f: A => B): List[B] = xs map f
