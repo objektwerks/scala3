ba3c15956ff500159de9db7a95bef63f7d9269ee
objektwerks <objektwerks@runbox.com>
2022-04-28 11:17:21 -0400

    added trait test

---
+++ src/test/scala/objektwerks/classes/TraitTest.scala
@@ -0,0 +1,13 @@
+package objektwerks.classes
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class TraitTest extends AnyFunSuite with Matchers:
+  test("constructor") {
+
+  }
+
+  test("super") {
+    
+  }
