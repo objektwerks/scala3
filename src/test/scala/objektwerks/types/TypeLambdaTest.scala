500da214877d99c2d746050f79cf3a08af0ba194
objektwerks <objektwerks@runbox.com>
2022-04-28 13:16:58 -0400

    added type lambda test

---
+++ src/test/scala/objektwerks/types/TypeLambdaTest.scala
@@ -0,0 +1,9 @@
+package objektwerks.types
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class TypeLambdaTest extends AnyFunSuite with Matchers:
+  test("type lambda") {
+    
+  }
