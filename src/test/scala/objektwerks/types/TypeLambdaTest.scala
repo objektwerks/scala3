131f8377c0be4f83b9257b464e52ac7054ae0178
objektwerks <objektwerks@runbox.com>
2022-04-28 13:26:58 -0400

    implemented type lambda test

--- src/test/scala/objektwerks/types/TypeLambdaTest.scala
+++ src/test/scala/objektwerks/types/TypeLambdaTest.scala
@@ -4,6 +4,10 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class TypeLambdaTest extends AnyFunSuite with Matchers:
+  type MapByStringKey = [T] =>> Map[String, T]
+
   test("type lambda") {
-    
+    var map: MapByStringKey[String] = Map()
+    map = map + ("a" -> "b")
+    map.get("a") shouldBe Some("b")
   }
