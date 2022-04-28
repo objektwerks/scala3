877b70b29c2033d09db5b4e85e28a6c9920316b0
objektwerks <objektwerks@runbox.com>
2022-04-28 11:38:48 -0400

    enhanced trait test

--- src/test/scala/objektwerks/traits/TraitTest.scala
+++ src/test/scala/objektwerks/traits/TraitTest.scala
@@ -30,3 +30,32 @@ class TraitTest extends AnyFunSuite with Matchers:
     val madamGoodEveningGreeter = Greeter(madam, goodEvening)
     madamGoodEveningGreeter.greet() shouldBe s"${madamGoodEveningGreeter.honorific}, ${madamGoodEveningGreeter.greeting}"
   }
+
+  trait Speaker(val topic: String):
+    def speak(): String
+
+  trait BoringSpeaker extends Speaker
+  trait ExcitingSpeaker extends Speaker
+
+  class Teacher(name: String) extends Speaker("math"):
+    override def speak(): String = "1 + 2 = 3"
+  
+  class Professor(name: String) extends BoringSpeaker with Speaker("biology"):
+    override def speak(): String = "your cells are powered by structured water"
+
+  class Coach(name: String) extends ExcitingSpeaker with Speaker("football"):
+    override def speak(): String = "go team go!"
+
+  test("constructor") {
+    val teacher = Teacher("fred flintstone")
+    teacher.topic shouldBe "math"
+    teacher.speak() shouldBe "1 + 2 = 3"
+
+    val professor = Professor("wilma flintstone")
+    professor.topic shouldBe "biology"
+    professor.speak() shouldBe "your cells are powered by structured water"
+
+    val coach = Coach("barney rebel")
+    coach.topic shouldBe "football"
+    coach.speak() shouldBe "go team go!"
+  }
