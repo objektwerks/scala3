29b47d51ffdb44046e8bd27d1287f75404a05e6c
objektwerks <objektwerks@runbox.com>
2022-04-28 13:53:19 -0400

    reformatted recurusion test code

--- src/test/scala/objektwerks/recursion/RecursionTest.scala
+++ src/test/scala/objektwerks/recursion/RecursionTest.scala
@@ -7,55 +7,47 @@ import scala.annotation.tailrec
 import scala.util.Try
 
 @tailrec
-def sum(xs: List[Int], acc: Int = 0): Int = xs match {
+def sum(xs: List[Int], acc: Int = 0): Int = xs match
   case Nil => acc
   case head :: tail => sum(tail, acc + head)
-}
 
 @tailrec
-def product(xs: List[Int], acc: Int = 1): Int = xs match {
+def product(xs: List[Int], acc: Int = 1): Int = xs match
   case Nil => acc
   case head :: tail => product(tail, acc * head)
-}
 
 @tailrec
-def reverse[A](list: List[A], acc: List[A] = List.empty[A]): List[A] = list match {
+def reverse[A](list: List[A], acc: List[A] = List.empty[A]): List[A] = list match
   case Nil => acc
   case head :: tail => reverse(tail, head :: acc)
-}
 
-def findNthElementFromRight[A](list: List[A], nthElement: Int): Option[A] = {
+def findNthElementFromRight[A](list: List[A], nthElement: Int): Option[A] =
   @tailrec
-  def reverse(list: List[A], acc: List[A] = List.empty[A]): List[A] = list match {
+  def reverse(list: List[A], acc: List[A] = List.empty[A]): List[A] = list match
     case Nil => acc
     case head :: tail => reverse(tail, head :: acc)
-  }
+  
   Try { reverse(list)(nthElement - 1) }.toOption
-}
 
 @tailrec
-final def factorial(n: Int, acc: Int = 1): Int = n match {
+final def factorial(n: Int, acc: Int = 1): Int = n match
   case i if i < 1 => acc
   case _ => factorial(n - 1, acc * n)
-}
 
-def fibonacci(n: Long): BigInt = {
+def fibonacci(n: Long): BigInt =
   @tailrec
-  def loop(n: Long, a: Long, b: Long): BigInt = n match {
+  def loop(n: Long, a: Long, b: Long): BigInt = n match
     case 0 => a
     case _ => loop(n - 1, b, a + b)
-  }
-  loop(n, 0, 1)
-}
 
-def isPrime(n: Int): Boolean = {
+  loop(n, 0, 1)
+
+def isPrime(n: Int): Boolean =
   @tailrec
-  def loop(current: Int): Boolean = {
+  def loop(current: Int): Boolean =
     if (current > Math.sqrt( Math.abs(n.toDouble)) ) true
     else n % current != 0 && loop(current + 1)
-  }
   if (n == -1 || n == 0 || n == 1) false else loop(2)
-}
 
 @tailrec
 final def intersectLists[A](listA: List[A],
