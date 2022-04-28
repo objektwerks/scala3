ff32adb4da9b21eb622fefb2997c20b425da2af7
objektwerks <objektwerks@runbox.com>
2022-04-28 11:48:18 -0400

    simplified trait test

--- src/test/scala/objektwerks/traits/TraitTest.scala
+++ src/test/scala/objektwerks/traits/TraitTest.scala
@@ -12,11 +12,8 @@ class TraitTest extends AnyFunSuite with Matchers:
     case goodMorning extends Greeting("I bid you good morning.")
     case goodEvening extends Greeting("I bid you good evening.")
 
-  transparent trait SalutationBuilder:
-    def build(honorific: Honorific, greeting: Greeting): String = s"$honorific, $greeting"
-
-  sealed trait Salutation(val honorific: Honorific, val greeting: Greeting) extends SalutationBuilder:
-    def greet(): String = build(honorific, greeting)
+  sealed trait Salutation(val honorific: Honorific, val greeting: Greeting):
+    def greet(): String = s"$honorific, $greeting"
 
   final class Greeter(honorific: Honorific, greeting: Greeting) extends Salutation(honorific, greeting)
 
@@ -31,19 +28,19 @@ class TraitTest extends AnyFunSuite with Matchers:
     madamGoodEveningGreeter.greet() shouldBe s"${madamGoodEveningGreeter.honorific}, ${madamGoodEveningGreeter.greeting}"
   }
 
-  trait Speaker(val topic: String):
+  sealed trait Speaker(val topic: String):
     def speak(): String
 
-  trait BoringSpeaker extends Speaker
-  trait ExcitingSpeaker extends Speaker
+  sealed trait BoringSpeaker extends Speaker
+  sealed trait ExcitingSpeaker extends Speaker
 
-  class Teacher(name: String) extends Speaker("math"):
+  final class Teacher(name: String) extends Speaker("math"):
     override def speak(): String = "1 + 2 = 3"
   
-  class Professor(name: String) extends BoringSpeaker with Speaker("biology"):
+  final class Professor(name: String) extends BoringSpeaker with Speaker("biology"):
     override def speak(): String = "your cells are powered by structured water"
 
-  class Coach(name: String) extends ExcitingSpeaker with Speaker("football"):
+  final class Coach(name: String) extends ExcitingSpeaker with Speaker("football"):
     override def speak(): String = "go team go!"
 
   test("constructor") {
