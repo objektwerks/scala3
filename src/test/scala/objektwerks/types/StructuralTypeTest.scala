03d15e5a9b057bdf7964d93b832285e41b06f319
objektwerks <objektwerks@runbox.com>
2022-04-29 14:00:27 -0400

    reformatted structural type test

--- src/test/scala/objektwerks/types/StructuralTypeTest.scala
+++ src/test/scala/objektwerks/types/StructuralTypeTest.scala
@@ -21,6 +21,7 @@ class StructuralTypeTest extends AnyFunSuite with Matchers:
     val make = auto.selectDynamic("make").asInstanceOf[String]
     val model = auto.selectDynamic("model").asInstanceOf[String]
     val year = auto.selectDynamic("year").asInstanceOf[Int]
+
     make shouldBe "Porsche"
     model shouldBe "911"
     year shouldBe 1964
