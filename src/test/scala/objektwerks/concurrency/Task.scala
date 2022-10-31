663fd79cc95e28795726cb3cfb51e5985ef6a688
objektwerks <objektwerks@runbox.com>
2022-10-31 12:59:15 -0400

    enhanced task

--- src/test/scala/objektwerks/concurrency/Task.scala
+++ src/test/scala/objektwerks/concurrency/Task.scala
@@ -1,6 +1,6 @@
 package objektwerks.concurrency
 
-import java.util.concurrent.Callable;
+import java.util.concurrent.Callable
 
 class FactorialTask(n: Int) extends Callable[Long]:
   def factorial(n: Int, acc: Int): Long =
@@ -8,3 +8,10 @@ class FactorialTask(n: Int) extends Callable[Long]:
       else factorial(n - 1, acc * n)
 
   def call(): Long = factorial(n, 1)
+
+class FibonacciTask(n: Int) extends Callable[Long]:
+  def fibonacci(n: Int, a: Int, b: Int): Long =
+    if (n == 0) then a
+    else fibonacci(n -1, b, a + b)
+
+  def call(): Long = fibonacci(n, 0, 1)
