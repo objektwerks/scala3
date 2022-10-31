9a37da808afe00f378fb8560bc76f1f5d7bb441d
objektwerks <objektwerks@runbox.com>
2022-10-31 12:48:54 -0400

    added virtual thread test

---
+++ src/test/scala/objektwerks/concurrency/VirtualThreadTest.scala
@@ -0,0 +1,9 @@
+package objektwerks.concurrency
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class VirtualThreadTest extends AnyFunSuite with Matchers:
+  test("virtual threads") {
+
+  }
