145e81d93716169f3e670b0ec1f2c6dd65b03a11
objektwerks <objektwerks@runbox.com>
2022-10-31 14:24:24 -0400

    refactored generic enum test

--- src/test/scala/objektwerks/enums/GenericEnumTest.scala
+++ src/test/scala/objektwerks/enums/GenericEnumTest.scala
@@ -3,23 +3,22 @@ package objektwerks.enums
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
+sealed trait Expr[T]
+final case class IntExpr(int: Int) extends Expr[Int]
+final case class BoolExpr(boolean: Boolean) extends Expr[Boolean]
+
+def eval[T](expr: Expr[T]): T =
+  expr match
+    case IntExpr(int) => int
+    case BoolExpr(boolean) => boolean
+
+enum Box[T](content: T):
+  case IntBox(number: Int) extends Box[Int](number)
+  case BoolBox(boolean: Boolean) extends Box[Boolean](boolean)
+
+  def unbox: T = content
+
 class GenericEnumTest extends AnyFunSuite with Matchers:
-  sealed trait Expr[T]
-  final case class IntExpr(int: Int) extends Expr[Int]
-  final case class BoolExpr(boolean: Boolean) extends Expr[Boolean]
-
-  def eval[T](expr: Expr[T]): T =
-    expr match
-      case IntExpr(int) => int
-      case BoolExpr(boolean) => boolean
-      case null => fail()
-
-  enum Box[T](content: T):
-    case IntBox(number: Int) extends Box[Int](number)
-    case BoolBox(boolean: Boolean) extends Box[Boolean](boolean)
-
-    def unbox: T = content
-
   test("trait > case class > gadt") {
     eval( IntExpr(3) ) shouldBe 3
     eval( BoolExpr(true) ) shouldBe true
