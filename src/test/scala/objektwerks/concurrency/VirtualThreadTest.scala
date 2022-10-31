09aca9fb29d89e9386ea1347b7791f467e3eb684
objektwerks <objektwerks@runbox.com>
2022-10-31 15:37:36 -0400

    edited imports

--- src/test/scala/objektwerks/concurrency/VirtualThreadTest.scala
+++ src/test/scala/objektwerks/concurrency/VirtualThreadTest.scala
@@ -1,9 +1,7 @@
 package objektwerks.concurrency
 
-import java.util.ArrayList;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.Executors
+import java.util.concurrent.Future
 
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
