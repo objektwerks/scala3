96ce3eaa9d2e828da66573a12b123f3f1f9aa819
objektwerks <objektwerks@runbox.com>
2022-10-31 13:37:37 -0400

    implemented virutal thread test; edited task

--- src/test/scala/objektwerks/concurrency/Task.scala
+++ src/test/scala/objektwerks/concurrency/Task.scala
@@ -2,14 +2,14 @@ package objektwerks.concurrency
 
 import java.util.concurrent.Callable
 
-class FactorialTask(n: Int) extends Callable[Long]:
+final class FactorialTask(n: Int) extends Callable[Long]:
   def factorial(n: Int, acc: Int): Long =
       if (n < 1) then acc
       else factorial(n - 1, acc * n)
 
   def call(): Long = factorial(n, 1)
 
-class FibonacciTask(n: Int) extends Callable[Long]:
+final class FibonacciTask(n: Int) extends Callable[Long]:
   def fibonacci(n: Int, a: Int, b: Int): Long =
     if (n == 0) then a
     else fibonacci(n -1, b, a + b)
--- src/test/scala/objektwerks/concurrency/VirtualThreadTest.scala
+++ src/test/scala/objektwerks/concurrency/VirtualThreadTest.scala
@@ -1,9 +1,28 @@
 package objektwerks.concurrency
 
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
+import scala.collection.mutable.ArrayBuffer
+import scala.jdk.CollectionConverters.*
+import scala.util.Using
+
 class VirtualThreadTest extends AnyFunSuite with Matchers:
   test("virtual threads") {
+    val tasks = ArrayBuffer.empty[FibonacciTask]
+    for(i <- 1 to 100) tasks += FibonacciTask(i)
 
+    var sum = 0L
+    Using.resource(Executors.newVirtualThreadPerTaskExecutor()) { executor =>
+      val futures = executor.invokeAll(tasks.asJava)
+      futures.asScala.foreach { future => sum += future.get() }
+    }
+
+    System.out.println("sum: " + Math.abs(sum));
+    assert(84580933396L == Math.abs(sum));
   }
