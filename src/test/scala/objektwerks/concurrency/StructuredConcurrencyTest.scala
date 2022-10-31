7c7bf2cac8bf2df0d864dca22d50782d0a8e01be
objektwerks <objektwerks@runbox.com>
2022-10-31 12:50:03 -0400

    added structured concurrency test

---
+++ src/test/scala/objektwerks/concurrency/StructuredConcurrencyTest.scala
@@ -0,0 +1,9 @@
+package objektwerks.concurrency
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class StructuredConcurrencyTest extends AnyFunSuite with Matchers:
+  test("structured concurrency") {
+
+  }
