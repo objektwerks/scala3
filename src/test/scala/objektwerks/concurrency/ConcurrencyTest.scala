3cd6977d62935ac5367c312b308ab4651341bfc1
objektwerks <objektwerks@runbox.com>
2022-11-01 01:17:54 -0400

    moved task into concurrency test

--- src/test/scala/objektwerks/concurrency/ConcurrencyTest.scala
+++ src/test/scala/objektwerks/concurrency/ConcurrencyTest.scala
@@ -1,6 +1,6 @@
 package objektwerks.concurrency
 
-import java.util.concurrent.{Executors, Future}
+import java.util.concurrent.{Callable, Executors, Future}
 import jdk.incubator.concurrent.StructuredTaskScope
 
 import org.scalatest.funsuite.AnyFunSuite
@@ -10,6 +10,20 @@ import scala.collection.mutable.ArrayBuffer
 import scala.jdk.CollectionConverters.*
 import scala.math.*
 import scala.util.{Failure, Success, Try, Using}
+
+final class FactorialTask(n: Int) extends Callable[Long]:
+  def factorial(n: Int, acc: Int): Long =
+      if (n < 1) then acc
+      else factorial(n - 1, acc * n)
+
+  def call(): Long = factorial(n, 1)
+
+final class FibonacciTask(n: Int) extends Callable[Long]:
+  def fibonacci(n: Int, a: Int, b: Int): Long =
+    if (n == 0) then a
+    else fibonacci(n -1, b, a + b)
+
+  def call(): Long = fibonacci(n, 0, 1)
 
 class ConcurrencyTest extends AnyFunSuite with Matchers:
   test("unstructured") {
