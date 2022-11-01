413545d8637ec32a01d87d51e929b3f5e1fbdad1
objektwerks <objektwerks@runbox.com>
2022-11-01 07:39:59 -0400

    refactored concurrency test to use file io

--- src/test/scala/objektwerks/concurrency/ConcurrencyTest.scala
+++ src/test/scala/objektwerks/concurrency/ConcurrencyTest.scala
@@ -8,23 +8,18 @@ import org.scalatest.matchers.should.Matchers
 
 import scala.annotation.tailrec
 import scala.collection.mutable.ArrayBuffer
+import scala.io.{Codec, Source}
 import scala.jdk.CollectionConverters.*
 import scala.math.*
 import scala.util.{Failure, Success, Try, Using}
 
-final class FactorialTask(n: Int) extends Callable[Long]:
-  @tailrec def factorial(n: Int, acc: Long = 1): Long = n match
-    case i if i < 1 => acc
-    case _ => factorial(n - 1, acc * n)
+final class FileLineCountTask(file: String) extends Callable[Int]:
+  def fileLineCount(file: String): Int = 
+    Using( Source.fromFile(file, Codec.UTF8.name) ) { source =>
+      source.getLines().length
+    }.get
 
-  def call(): Long = factorial(n)
-
-final class FibonacciTask(n: Int) extends Callable[Long]:
-  @tailrec def fibonacci(n: Int, a: Long = 0, b: Long = 1): Long = n match
-    case 0 => a
-    case _ => fibonacci(n - 1, b, a + b)
-
-  def call(): Long = fibonacci(n)
+  def call(): Int = fileLineCount(file)
 
 /**
   * Virtual Threads: https://openjdk.org/jeps/425
@@ -32,8 +27,9 @@ final class FibonacciTask(n: Int) extends Callable[Long]:
   */
 class ConcurrencyTest extends AnyFunSuite with Matchers:
   test("virtual threads") {
-    val tasks = ArrayBuffer.empty[FibonacciTask]
-    for(i <- 1 to 30) tasks += FibonacciTask(i)
+    val tasks = ArrayBuffer.empty[FileLineCountTask]
+    tasks += FileLineCountTask("./LICENSE")
+    tasks += FileLineCountTask("./.gitignore")
 
     val result: Try[Long] = Using(Executors.newVirtualThreadPerTaskExecutor()) { executor =>
       val futures = executor.invokeAll(tasks.asJava)
@@ -41,14 +37,14 @@ class ConcurrencyTest extends AnyFunSuite with Matchers:
     }
 
     result match
-      case Success(sum) => assert(2178308 == abs(sum))
+      case Success(sum) => assert(54 == abs(sum))
       case Failure(error) => fail(error.getMessage())
   }
 
   test("structured concurrency") {
     val result: Try[Long] = Using (new StructuredTaskScope.ShutdownOnFailure()) { scope =>
-      val factorial = scope.fork(() => new FactorialTask(10).call())
-      val fibonacci = scope.fork(() => new FibonacciTask(20).call())
+      val factorial = scope.fork(() => new FileLineCountTask("./LICENSE").call())
+      val fibonacci = scope.fork(() => new FileLineCountTask("./.gitignore").call())
 
       scope.join();
       scope.throwIfFailed();
@@ -57,6 +53,6 @@ class ConcurrencyTest extends AnyFunSuite with Matchers:
     }
 
     result match
-      case Success(sum) => assert(3635565 == abs(sum))
+      case Success(sum) => assert(54 == abs(sum))
       case Failure(error) => fail(error.getMessage())
   }
