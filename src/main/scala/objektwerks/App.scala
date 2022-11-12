5e33a0764367fd2dcc17f055eb2a21be76c57b07
objektwerks <objektwerks@runbox.com>
2022-11-12 10:23:04 -0500

    edited app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -2,6 +2,4 @@ package objektwerks
 
 object App:
   @main def greeting(first: String, last: String): Unit =
-    println(">>>")
     println(s"@main greetings(first: String, last: String) => Greetings, $first $last!")
-    println("<<<")
