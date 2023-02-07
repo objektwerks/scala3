bb36f3d520a7424f6f04c0c85e44c44ab00fdac0
objektwerks <objektwerks@runbox.com>
2023-02-07 13:09:31 -0500

    added scaladoc to app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -1,5 +1,9 @@
 package objektwerks
 
+/**
+ * Select Modify Run Configuration... and provide CLI args, such as: James Bond
+ * then select the run triangle ( in the left margin ) and run 'greeting'
+ */
 object App:
   @main def greeting(first: String, last: String): Unit =
     println(s"@main greetings(first: String, last: String) => Greetings, $first $last!")
