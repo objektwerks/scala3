85548eb28f11311744c8ac36c9f3d1e22678e72e
objektwerks <objektwerks@runbox.com>
2023-02-07 13:12:04 -0500

    edited scaladoc in app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -1,8 +1,10 @@
 package objektwerks
 
 /**
- * Select Modify Run Configuration... and provide CLI args, such as: James Bond
- * then select the run triangle ( in the left margin ) and run 'greeting'
+ * To run via IDE:
+ * 1. select run triangle ( in left margin ) > Modify Run Configuration...
+ * 2. provide CLI args, such as: James Bond
+ * 3. select the run triangle > run 'greeting'
  */
 object App:
   @main def greeting(first: String, last: String): Unit =
