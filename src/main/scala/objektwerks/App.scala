210de5716d13e817f766a04f883e8f207426baad
objektwerks <objektwerks@runbox.com>
2023-02-07 13:14:55 -0500

    edited scaladoc in app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -3,8 +3,11 @@ package objektwerks
 /**
  * To run via IDE:
  * 1. select run triangle ( in left margin ) > Modify Run Configuration...
- * 2. provide CLI args, such as: James Bond
+ * 2. provide CLI args, such as: Fred Flintstone
  * 3. select run triangle > run 'greeting'
+ *
+ * To run via terminal:
+ * 1. sbt "run Fred Flintstone"
  */
 object App:
   @main def greeting(first: String, last: String): Unit =
