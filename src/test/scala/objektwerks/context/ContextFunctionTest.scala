1f01c864f4ffea5af7ec8b7ddada6eae0387d3ed
objektwerks <objektwerks@runbox.com>
2023-04-28 06:08:19 -0400

    refactored context function test

--- src/test/scala/objektwerks/context/ContextFunctionTest.scala
+++ src/test/scala/objektwerks/context/ContextFunctionTest.scala
@@ -9,11 +9,11 @@ import scala.concurrent.{ExecutionContext, Future}
   * See: https://blog.softwaremill.com/context-is-king-20f533474cb3
   */
 class ContextFunctionTest extends AnyFunSuite with Matchers:
-  type Executable[T] = ExecutionContext ?=> T
+  type Executable[T] = ExecutionContext ?=> Future[T]
 
   given ec: ExecutionContext = ExecutionContext.global
 
-  def square(n: Int): Executable[Future[Int]] = Future { n * n }
+  def square(n: Int): Executable[Int] = Future { n * n }
 
   test("?=>") {
     square(2).foreach( result => result shouldBe 4 )
