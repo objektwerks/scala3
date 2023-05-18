ab2a7d6390f757ca0573132c42324a086896f4ad
objektwerks <objektwerks@runbox.com>
2023-05-18 07:24:02 -0400

    enhanced context function test

--- src/test/scala/objektwerks/context/ContextFunctionTest.scala
+++ src/test/scala/objektwerks/context/ContextFunctionTest.scala
@@ -4,6 +4,7 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 import scala.concurrent.{ExecutionContext, Future}
+import java.util.UUID
 
 /**
   * See: https://blog.softwaremill.com/context-is-king-20f533474cb3
@@ -15,6 +16,16 @@ class ContextFunctionTest extends AnyFunSuite with Matchers:
 
   def square(n: Int): Executable[Int] = Future { n * n }
 
-  test("?=>") {
+  test("executable context") {
     square(2).foreach( result => result shouldBe 4 )
+  }
+
+  final case class Authorization(id: String = UUID.randomUUID.toString)
+
+  type AuthorizationContext[T] = Authorization ?=> T
+
+  def login(pin: String): AuthorizationContext[Authorization] = Authorization()
+
+  test("authorization context") {
+
   }
