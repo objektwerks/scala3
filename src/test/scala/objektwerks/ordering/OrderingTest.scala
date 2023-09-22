261c75756dd06fe5dba5e18335d6bcc4e63c788b
objektwerks <objektwerks@runbox.com>
2023-09-22 11:16:02 -0400

    edited ordering test

--- src/test/scala/objektwerks/ordering/OrderingTest.scala
+++ src/test/scala/objektwerks/ordering/OrderingTest.scala
@@ -3,20 +3,20 @@ package objektwerks.ordering
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-class OrderingTest extends AnyFunSuite with Matchers:
+final class OrderingTest extends AnyFunSuite with Matchers:
   final case class Name(last: String, first: String)
 
-  given lastNameOrdering: Ordering[Name] = Ordering.by(_.last)
-  given firstNameOrdering: Ordering[Name] = Ordering.by(_.first)
+  given lastOrdering: Ordering[Name] = Ordering.by(_.last)
+  given firstOrdering: Ordering[Name] = Ordering.by(_.first)
 
   test("ordering > sorted") {
     val aName = Name(last = "a", first = "z")
     val zName = Name(last = "z", first = "a")
     val names = Seq( aName, zName )
 
-    val lastNameSorted = names.sorted[Name](using lastNameOrdering)
+    val lastNameSorted = names.sorted[Name](using lastOrdering)
     lastNameSorted.head shouldBe aName
 
-    val firstNameSorted = names.sorted[Name](using firstNameOrdering)
+    val firstNameSorted = names.sorted[Name](using firstOrdering)
     firstNameSorted.head shouldBe zName
   }
