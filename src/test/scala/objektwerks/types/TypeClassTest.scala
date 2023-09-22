922898b62046c48e76f8759d16974d94b3c5200c
objektwerks <objektwerks@runbox.com>
2023-09-22 11:44:34 -0400

    edited type class test

--- src/test/scala/objektwerks/types/TypeClassTest.scala
+++ src/test/scala/objektwerks/types/TypeClassTest.scala
@@ -3,7 +3,7 @@ package objektwerks.types
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-class TypeClassTest extends AnyFunSuite with Matchers:
+final class TypeClassTest extends AnyFunSuite with Matchers:
   sealed trait SemiGroup[T]:
     extension (a: T) def join (b: T): T
 
