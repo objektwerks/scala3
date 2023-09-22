eda1f31933c9118029d55885228aac37eed3e239
objektwerks <objektwerks@runbox.com>
2023-09-22 11:49:15 -0400

    edited type lambda test

--- src/test/scala/objektwerks/types/TypeLambdaTest.scala
+++ src/test/scala/objektwerks/types/TypeLambdaTest.scala
@@ -3,7 +3,10 @@ package objektwerks.types
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-class TypeLambdaTest extends AnyFunSuite with Matchers:
+/**
+  * See: https://blog.rockthejvm.com/scala-3-type-lambdas/
+  */
+final class TypeLambdaTest extends AnyFunSuite with Matchers:
   type MapByStringKey = [T] =>> Map[String, T]
 
   test("type lambda") {
