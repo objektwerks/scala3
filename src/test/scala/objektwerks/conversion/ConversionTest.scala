32b4892857f5adc638a6e73145a270d31cc8477d
objektwerks <objektwerks@runbox.com>
2023-09-22 10:46:43 -0400

    edited conversion test

--- src/test/scala/objektwerks/conversion/ConversionTest.scala
+++ src/test/scala/objektwerks/conversion/ConversionTest.scala
@@ -5,11 +5,11 @@ import org.scalatest.matchers.should.Matchers
     
 import scala.language.implicitConversions
 
-class ConversionTest extends AnyFunSuite with Matchers:
-  case class Person(name: String):
+final class ConversionTest extends AnyFunSuite with Matchers:
+  final case class Person(name: String):
     def greetings: String = s"Greetings. My name is $name."
 
-  given stringToPerson: Conversion[String, Person] with
+  given Conversion[String, Person] with
     def apply(name: String): Person = Person(name)
 
   test("conversion") {
