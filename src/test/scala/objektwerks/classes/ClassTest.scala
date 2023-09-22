fdcb1c2fd2eed295d158f7d88fe7d5a13fa493ef
objektwerks <objektwerks@runbox.com>
2023-09-22 10:22:38 -0400

    refactored class test

--- src/test/scala/objektwerks/classes/ClassTest.scala
+++ src/test/scala/objektwerks/classes/ClassTest.scala
@@ -3,7 +3,7 @@ package objektwerks.classes
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-class ClassTest extends AnyFunSuite with Matchers:
+final class ClassTest extends AnyFunSuite with Matchers:
   sealed trait Car:
     def drive: String = "driving"
     def sound: String
@@ -15,25 +15,25 @@ class ClassTest extends AnyFunSuite with Matchers:
   object Owner:
     def startEngines: Set[Car] = Set( Porsche("prrrr"), Corvette("woosh"), Maserati("grrrr") )
 
-  class Human(val first: String, val last: String, val initial: String):
+  final class Human(val first: String, val last: String, val initial: String):
     def this(first: String, last: String) = this(first, last, "")
 
-  class Square:
+  final class Square:
     def apply(n: Int): Int = n * n
 
   object Cube:
     def apply(n: Int): Int = n * n * n
 
-  case class Timestamp(seconds: Int)
+  final case class Timestamp(seconds: Int)
 
   object Timestamp:
     def apply(hours: Int, minutes: Int, seconds: Int): Timestamp = Timestamp( (hours * 60 * 60) + (minutes * 60) + seconds )
 
   test("classes with inheritence") {
     val cars = Owner.startEngines
-    for (car <- cars) {
+    for (car <- cars)
       car.sound.nonEmpty shouldBe true
-      car match {
+      car match
         case porsche: Porsche =>
           porsche.drive shouldEqual "driving"
           porsche.sound shouldEqual "prrrr"
@@ -43,19 +43,17 @@ class ClassTest extends AnyFunSuite with Matchers:
         case maserati: Maserati =>
           maserati.drive shouldEqual "driving"
           maserati.sound shouldEqual "grrrr"
-      }
-    }
   }
 
   test("constructors") {
-    val primary = new Human("fred", "flintstone", "r")
-    val secondary = new Human("barney", "rebel")
+    val primary = Human("fred", "flintstone", "r")
+    val secondary = Human("barney", "rebel")
     primary.initial.nonEmpty shouldBe true
     secondary.initial.isEmpty shouldBe true
   }
 
   test("class apply") {
-    val square = new Square()
+    val square = Square()
     square(2) shouldEqual 4
     square.apply(3) shouldEqual 9
   }
