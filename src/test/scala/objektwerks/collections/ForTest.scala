f8750dcf337dfa771a26f48e05e037048c4dfeb5
objektwerks <objektwerks@runbox.com>
2023-09-22 10:29:00 -0400

    refactored for test

--- src/test/scala/objektwerks/collections/ForTest.scala
+++ src/test/scala/objektwerks/collections/ForTest.scala
@@ -6,7 +6,7 @@ import org.scalatest.matchers.should.Matchers
 import scala.collection.mutable
 import scala.concurrent.{ExecutionContext, Future}
 
-class ForTest extends AnyFunSuite with Matchers:
+final class ForTest extends AnyFunSuite with Matchers:
   test("foreach") {
     val map = Map(1 -> 1, 2 -> 2, 3 -> 3)
     map.foreach( (key, value) => (key > 0) && (value > 0) shouldBe true )
@@ -35,29 +35,28 @@ class ForTest extends AnyFunSuite with Matchers:
 
   test("for comprehension") {
     val xs = List(1, 2, 3)
-    val ys = for {
+    val ys = for
       x <- xs
-    } yield x * 2
+    yield x * 2
     ys shouldEqual xs.map(_ * 2)
 
     val as = List(List(1), List(2, 3), List(4, 5, 6))
-    val bs = for {
+    val bs = for
       sas <- as
       a <- sas
-    } yield a * 2
+    yield a * 2
     bs shouldEqual as.flatMap(_.map( _ * 2 ))
   }
 
   test("nested for comprehensions") {
     val xs = List(1, 2, 3)
     val ys = List(4, 5, 6)
-    val zs = for {
+    val zs = for
       x <- xs
-    } yield {
-      for {
+    yield
+      for
         y <- ys
-      } yield x + y
-    }
+      yield x + y
     zs shouldEqual List(List(5, 6, 7), List(6, 7, 8), List(7, 8, 9))
     zs.flatten shouldEqual List(5, 6, 7, 6, 7, 8, 7, 8, 9)
     zs.flatten.sum shouldEqual 63
@@ -65,9 +64,9 @@ class ForTest extends AnyFunSuite with Matchers:
 
   test("for comprehension > map") {
     val o = Option(3)
-    val c = for {
+    val c = for
       x <- o map { i => i * i * i }
-    } yield x
+    yield x
     c.get shouldEqual 27
   }
 
@@ -75,10 +74,10 @@ class ForTest extends AnyFunSuite with Matchers:
     val xs = List(2, 4)
     val ys = List(3, 5)
     
-    val forList = for {
+    val forList = for
       x <- xs
       y <- ys
-    } yield x * y
+    yield x * y
 
     val mapList = xs flatMap { x =>
       ys map { y =>
@@ -95,11 +94,11 @@ class ForTest extends AnyFunSuite with Matchers:
     val ys = List(3, 5)
     val zs = List(1, 6)
 
-    val forList = for {
+    val forList = for
       x <- xs
       y <- ys
       z <- zs
-     } yield x * y * z
+     yield x * y * z
 
     val mapList = xs flatMap { x =>
       ys flatMap { y =>
@@ -115,31 +114,31 @@ class ForTest extends AnyFunSuite with Matchers:
 
   test("for comprehension > if guard filter") {
     val ls = List("A", "B", "C", "D", "F")
-    val filteredLetters = for {
+    val filteredLetters = for
       l <- ls if l == "A"
-    } yield l
+    yield l
 
     val ns = List(-2, -1, 0, 1, 2)
-    val filteredNumbers = for {
+    val filteredNumbers = for
       n <- ns if n > 0
-    } yield n
+    yield n
 
     filteredLetters.head shouldEqual "A"
     filteredNumbers shouldEqual List(1, 2)
   }
 
   test("for comprehension > zip") {
-    val xs = for {
+    val xs = for
       (a, b) <- List(1, 2, 3) zip List(4, 5, 6)
-    } yield a + b
+    yield a + b
     xs shouldEqual List(5, 7, 9)
   }
 
   test("for comprehension with recover") {
     given ec: ExecutionContext = ExecutionContext.global
     val future = Future(Integer.parseInt("one"))
-    val result = ( for {
+    val result = ( for
       i <- future 
-    } yield i ).recover { case _: Throwable => -1 }
+    yield i ).recover { case _: Throwable => -1 }
     result foreach { x => x shouldEqual -1 }
   }
