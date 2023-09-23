0fc9175300f4fdd0250a348a2ebc32ab15256c53
objektwerks <objektwerks@runbox.com>
2023-09-23 09:08:31 -0400

    added boundary break test

---
+++ src/test/scala/objektwerks/control/BoundaryBreakTest.scala
@@ -0,0 +1,9 @@
+package objektwerks.control
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class BoundaryBreakTest extends AnyFunSuite with Matchers:
+  test("boundary > break") {
+    
+  }
