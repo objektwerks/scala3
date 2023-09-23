d633bc439a771955055c72c42d4ace8ff0a30abf
objektwerks <objektwerks@runbox.com>
2023-09-23 09:48:59 -0400

    reformatted test

--- src/test/scala/objektwerks/control/BoundaryBreakTest.scala
+++ src/test/scala/objektwerks/control/BoundaryBreakTest.scala
@@ -6,11 +6,12 @@ import org.scalatest.matchers.should.Matchers
 import util.boundary, boundary.break
 
 class BoundaryBreakTest extends AnyFunSuite with Matchers:
-  def sumOfRoots(ns: List[Long]): Option[Long] = boundary:
-    val roots = ns.map: n => 
-      if n >= 0 then Math.sqrt(n)
-      else break(None)
-    Some(roots.sum.round)
+  def sumOfRoots(ns: List[Long]): Option[Long] =
+    boundary:
+      val roots = ns.map: n => 
+        if n >= 0 then Math.sqrt(n)
+        else break(None)
+      Some(roots.sum.round)
 
   test("boundary > break"):
     sumOfRoots( List(4, 8) ) shouldBe Some(5)
