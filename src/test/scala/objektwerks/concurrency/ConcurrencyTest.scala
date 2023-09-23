d6a506699d7856834bc2d6ef9bbb36768bf5c6d3
objektwerks <objektwerks@runbox.com>
2023-09-23 09:45:07 -0400

    reformatted test

--- src/test/scala/objektwerks/concurrency/ConcurrencyTest.scala
+++ src/test/scala/objektwerks/concurrency/ConcurrencyTest.scala
@@ -24,51 +24,45 @@ final class ConcurrencyTest extends AnyFunSuite:
   val tasks = List( FileLineCountTask("./data/data.a.csv"), FileLineCountTask("./data/data.b.csv") )
   val expectedLineCount = 540_959
 
-  test("virtual threads submit") {
+  test("virtual threads submit"):
     Using( Executors.newVirtualThreadPerTaskExecutor() ) { executor =>
       val aFuture = executor.submit( () => FileLineCountTask("./data/data.a.csv").call() )
       val bFuture = executor.submit( () => FileLineCountTask("./data/data.b.csv").call() )
       aFuture.get() + bFuture.get()
     }.fold( error => fail(error.getMessage), lines => assert(lines == expectedLineCount) )
-  }
 
-  test("virtual threads invoke all") {
+  test("virtual threads invoke all"):
     Using( Executors.newVirtualThreadPerTaskExecutor() ) { executor =>
       executor.invokeAll( tasks.asJava ).asScala.map( future => future.get() ).sum
     }.fold( error => fail(error.getMessage), lines => assert(lines == expectedLineCount) )
-  }
 
-  test("structured concurrency join") {
-    val lines = Using( StructuredTaskScope.ShutdownOnFailure() ) { scope =>
+  test("structured concurrency join"):
+    val lines = Using( StructuredTaskScope.ShutdownOnFailure() ): scope =>
       val alines = scope.fork( () => FileLineCountTask("./data/data.a.csv").call() )
       val blines = scope.fork( () => FileLineCountTask("./data/data.b.csv").call() )
       scope.join()
       scope.throwIfFailed()
       alines.get() + blines.get()
-    }
     lines match
       case Success(count) => assert(count == expectedLineCount)
       case Failure(error) => fail(error.getMessage)
-  }
 
-  test("structured concurrency join until") {
+  test("structured concurrency join until"):
     Using( StructuredTaskScope.ShutdownOnFailure() ) { scope =>
       val futures = tasks.map( task => scope.fork( () => task.call() ) )
       scope.joinUntil( Instant.now().plusMillis(3000) )
       scope.throwIfFailed()
       futures.map( future => future.get() ).sum
     }.fold( error => fail(error.getMessage), lines => assert(lines == expectedLineCount) )
-  }
 
-  test("structured concurrency race") {
+  test("structured concurrency race"):
     Using( StructuredTaskScope.ShutdownOnSuccess[Int]() ) { scope =>
       tasks.foreach( task => scope.fork( () => task.call() ) )
       scope.joinUntil( Instant.now().plusMillis(3000) )
       scope.result()
     }.fold( error => fail(error.getMessage), lines => assert(lines == 270_562 || lines == 270_397) )
-  }
 
-  test("scoped value") {
+  test("scoped value"):
     val license: ScopedValue[String] = ScopedValue.newInstance()
     val uuid = UUID.randomUUID.toString
     val count = ScopedValue
@@ -76,4 +70,3 @@ final class ConcurrencyTest extends AnyFunSuite:
       .call { () => if license.get.nonEmpty then 1 else -1 }
     assert(count == 1)
     assert(!license.isBound)
-  }
