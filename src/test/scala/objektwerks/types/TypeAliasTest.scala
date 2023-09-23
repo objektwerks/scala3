ab877663afa5e3541b216ee3c11ca7436d47dd48
objektwerks <objektwerks@runbox.com>
2023-09-23 10:38:57 -0400

    reformatted test

--- src/test/scala/objektwerks/types/TypeAliasTest.scala
+++ src/test/scala/objektwerks/types/TypeAliasTest.scala
@@ -4,11 +4,10 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 final class TypeAliasTest extends AnyFunSuite with Matchers:
-  test("type alias") {
+  test("type alias"):
     type User = String
     type Age = Int
     val users =  Map[User, Age]("john" -> 21, "jane" -> 19)
 
     users("john") shouldEqual 21
     users("jane") shouldEqual 19
-  }
