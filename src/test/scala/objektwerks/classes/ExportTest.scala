10e44eb7f0210a051ae2bc338627ad147813a004
objektwerks <objektwerks@runbox.com>
2023-10-23 11:43:03 -0400

    added export test

---
+++ src/test/scala/objektwerks/classes/ExportTest.scala
@@ -0,0 +1,8 @@
+package objektwerks.classes
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+final class ExportTest extends AnyFunSuite with Matchers:
+  test("export"):
+    println("test export")
