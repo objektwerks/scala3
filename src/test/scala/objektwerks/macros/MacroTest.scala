b685982536d534ddd92b0a2b2abf540990a353be
objektwerks <objektwerks@runbox.com>
2023-10-23 12:11:58 -0400

    added macro test

---
+++ src/test/scala/objektwerks/macros/MacroTest.scala
@@ -0,0 +1,8 @@
+package objektwerks.macros
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+final class MacroTest extends AnyFunSuite with Matchers:
+  test("macro"):
+    println("todo")
