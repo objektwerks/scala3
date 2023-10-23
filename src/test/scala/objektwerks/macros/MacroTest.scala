5d5b26c8235bfd076111e088c9db1e8f24c8db37
objektwerks <objektwerks@runbox.com>
2023-10-23 13:43:56 -0400

    edited macro test

--- src/test/scala/objektwerks/macros/MacroTest.scala
+++ src/test/scala/objektwerks/macros/MacroTest.scala
@@ -5,16 +5,18 @@ import org.scalatest.matchers.should.Matchers
 
 import scala.quoted.{Expr, Quotes}
 
-/**
- * See: https://www.baeldung.com/scala/macros-scala-3
- */
-final class MacroTest extends AnyFunSuite with Matchers:
-  def oddOrEven(n: Expr[Int])(using Quotes): Expr[String] = '{
+object OddOrEven:
+  def oddOrEvenQuotes(n: Expr[Int])(using Quotes): Expr[String] = '{
     $n % 2 match
       case 0 => "even"
       case _ => "odd"
   }
 
+  inline def oddOrEvenSplicing(inline n: Int): String = ${ oddOrEvenQuotes( 'n ) }
 
+/**
+ * See: https://www.baeldung.com/scala/macros-scala-3
+ */
+final class MacroTest extends AnyFunSuite with Matchers:
   test("macro"):
     println("todo")
