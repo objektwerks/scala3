ff75c1d43a333adb03ee8c1435de8514c48cd2bf
objektwerks <objektwerks@runbox.com>
2023-10-23 15:01:51 -0400

    edited scaladoc

--- src/test/scala/objektwerks/macros/MacroTest.scala
+++ src/test/scala/objektwerks/macros/MacroTest.scala
@@ -7,6 +7,7 @@ import org.scalatest.matchers.should.Matchers
  * See:
  * 1. https://www.baeldung.com/scala/macros-scala-3
  * 2. https://softwaremill.com/scala-3-macros-tips-and-tricks/
+ * 3. https://macros.scala.school/
  */
 final class MacroTest extends AnyFunSuite with Matchers:
   test("macro"):
