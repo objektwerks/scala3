ff7c3aa8fc79201485dc1032553a9c7beda30829
objektwerks <objektwerks@runbox.com>
2023-10-23 19:17:13 -0400

    extracted macros from object

--- src/test/scala/objektwerks/macros/OddOrEvenMacro.scala
+++ src/test/scala/objektwerks/macros/OddOrEvenMacro.scala
@@ -2,13 +2,12 @@ package objektwerks.macros
 
 import scala.quoted.{Expr, Quotes}
 
-object OddOrEvenMacro:
-  // Quote - builds an AST fragment
-  def oddOrEvenMacro(n: Expr[Int])(using Quotes): Expr[String] = '{
-    $n % 2 match
-      case 0 => "even"
-      case _ => "odd"
-  }
+// Quote - builds an AST fragment
+def oddOrEvenMacro(n: Expr[Int])(using Quotes): Expr[String] = '{
+  $n % 2 match
+    case 0 => "even"
+    case _ => "odd"
+}
 
-  // Splice - inserts an AST fragment into the program AST
-  inline def oddOrEven(inline n: Int): String = ${ OddOrEvenMacro.oddOrEvenMacro( 'n ) }
+// Splice - inserts an AST fragment into the program AST
+inline def oddOrEven(inline n: Int): String = ${ oddOrEvenMacro( 'n ) }
