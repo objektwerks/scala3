cfb7dac4ab1de0b4c808fe2534ff7c55a36938de
objektwerks <objektwerks@runbox.com>
2023-10-23 12:07:40 -0400

    enhanced export test

--- src/test/scala/objektwerks/classes/ExportTest.scala
+++ src/test/scala/objektwerks/classes/ExportTest.scala
@@ -5,24 +5,24 @@ import org.scalatest.matchers.should.Matchers
 
 import scala.collection.mutable
 
-final class CacheImpl:
-  private val map = mutable.Map.empty[Int, Int]
-  given secret: String = "secret"
-
-  def put(key: Int, value: Int): Option[Int] = map.put(key, value)
-  def get(key: Int): Option[Int] = map.get(key)
-  def size: Int = map.size
-  def clear(): Unit = map.clear()
-
-final class Cache:
-  private val cache = CacheImpl()
-  export cache.* // export all CacheImpl methods
-  export cache.given // export all given fields
-
 /**
  * See: https://www.baeldung.com/scala/scala-3-export
  */
 final class ExportTest extends AnyFunSuite with Matchers:
+  final class CacheImpl:
+    private val map = mutable.Map.empty[Int, Int]
+    given secret: String = "secret"
+
+    def put(key: Int, value: Int): Option[Int] = map.put(key, value)
+    def get(key: Int): Option[Int] = map.get(key)
+    def size: Int = map.size
+    def clear(): Unit = map.clear()
+
+  final class Cache:
+    private val cache = CacheImpl()
+    export cache.* // export all CacheImpl methods
+    export cache.given // export all given fields
+
   test("export"):
     val cache = Cache()
     cache.put(1, 1) shouldBe None
