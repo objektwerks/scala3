df09efd85806e5e72a26fda422e485c0ab065fdd
objektwerks <objektwerks@runbox.com>
2023-10-23 12:11:07 -0400

    added inline test

---
+++ src/test/scala/objektwerks/macros/InlineTest.scala
@@ -0,0 +1,8 @@
+package objektwerks.macros
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+final class InlineTest extends AnyFunSuite with Matchers:
+  test("inline"):
+    println("todo")
