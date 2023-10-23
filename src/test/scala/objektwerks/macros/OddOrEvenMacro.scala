cf66fd27206c58372e731b975c7dcce590ac5135
objektwerks <objektwerks@runbox.com>
2023-10-23 15:48:00 -0400

    added comment

--- src/test/scala/objektwerks/macros/OddOrEvenMacro.scala
+++ src/test/scala/objektwerks/macros/OddOrEvenMacro.scala
@@ -3,10 +3,12 @@ package objektwerks.macros
 import scala.quoted.{Expr, Quotes}
 
 object OddOrEvenMacro:
+  // Quotes
   def oddOrEvenMacro(n: Expr[Int])(using Quotes): Expr[String] = '{
     $n % 2 match
       case 0 => "even"
       case _ => "odd"
   }
 
+  // Splicing
   inline def oddOrEven(inline n: Int): String = ${ OddOrEvenMacro.oddOrEvenMacro( 'n ) }
