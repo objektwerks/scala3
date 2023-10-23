133899e13d33f045cba184bfa4ed71cc37cfd04e
objektwerks <objektwerks@runbox.com>
2023-10-23 12:51:39 -0400

    enhanced inline test

--- src/test/scala/objektwerks/macros/InlineTest.scala
+++ src/test/scala/objektwerks/macros/InlineTest.scala
@@ -7,5 +7,9 @@ import org.scalatest.matchers.should.Matchers
  * See: https://www.baeldung.com/scala/macros-scala-3
  */
 final class InlineTest extends AnyFunSuite with Matchers:
+  // inline def and parameter
+  inline def oddOrEven(inline n: Int): String = if n % 2 == 0 then "even" else "odd"
+
   test("inline"):
-    println("todo")
+    oddOrEven(2) shouldBe "even"
+    oddOrEven(1) shouldBe "odd"
