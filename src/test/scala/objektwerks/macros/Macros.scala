3338585c0f9db994f90fa0f856fc03b1bc3d39b7
objektwerks <objektwerks@runbox.com>
2023-10-23 19:18:41 -0400

    renamed macro method

--- src/test/scala/objektwerks/macros/Macros.scala
+++ src/test/scala/objektwerks/macros/Macros.scala
@@ -3,11 +3,11 @@ package objektwerks.macros
 import scala.quoted.{Expr, Quotes}
 
 // Quote - builds an AST fragment
-def oddOrEvenMacro(n: Expr[Int])(using Quotes): Expr[String] = '{
+def oddOrEvenImpl(n: Expr[Int])(using Quotes): Expr[String] = '{
   $n % 2 match
     case 0 => "even"
     case _ => "odd"
 }
 
 // Splice - inserts an AST fragment into the program AST
-inline def oddOrEven(inline n: Int): String = ${ oddOrEvenMacro( 'n ) }
+inline def oddOrEven(inline n: Int): String = ${ oddOrEvenImpl( 'n ) }
