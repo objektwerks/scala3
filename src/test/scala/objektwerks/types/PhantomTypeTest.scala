0f5cc551739e36b92859281ce9b58b2c93486a58
objektwerks <objektwerks@runbox.com>
2023-10-25 08:14:21 -0400

    enhanced phantom type test

--- src/test/scala/objektwerks/types/PhantomTypeTest.scala
+++ src/test/scala/objektwerks/types/PhantomTypeTest.scala
@@ -7,5 +7,12 @@ import org.scalatest.matchers.should.Matchers
   * See: https://macros.scala.school/phantom-types
   */
 class PhantomTypeTest extends AnyFunSuite with Matchers:
+  final case class Code[A] private (value: String):
+    def add(that: Code[Int]): Code[Int] = Code(s"($value + ${that.value})")
+
+  object Code:
+    def int(value: Int): Code[Int] = Code(value.toString)
+    def bool(value: Boolean): Code[Boolean] = Code(value.toString)
+
   test("phantom"):
     println("todo")
