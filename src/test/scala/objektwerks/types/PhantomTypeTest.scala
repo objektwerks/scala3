a24fb885555b66e5bab014a49998b589e0dbff4b
objektwerks <objektwerks@runbox.com>
2023-10-25 07:52:44 -0400

    added phantom type test

---
+++ src/test/scala/objektwerks/types/PhantomTypeTest.scala
@@ -0,0 +1,8 @@
+package objektwerks.types
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class PhantomTypeTest extends AnyFunSuite with Matchers:
+  test("phantom"):
+    println("todo")
