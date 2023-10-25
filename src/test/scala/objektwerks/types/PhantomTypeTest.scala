47c509dd70cdb24a1d0b6732b8ca1d6167ed27a3
objektwerks <objektwerks@runbox.com>
2023-10-25 07:53:37 -0400

    added scaladoc

--- src/test/scala/objektwerks/types/PhantomTypeTest.scala
+++ src/test/scala/objektwerks/types/PhantomTypeTest.scala
@@ -3,6 +3,9 @@ package objektwerks.types
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
+/**
+  * See: https://macros.scala.school/phantom-types
+  */
 class PhantomTypeTest extends AnyFunSuite with Matchers:
   test("phantom"):
     println("todo")
