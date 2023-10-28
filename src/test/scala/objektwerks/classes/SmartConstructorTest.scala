ce2288b553bfa99968ce4664b49f7d7999d629da
objektwerks <objektwerks@runbox.com>
2023-10-28 15:46:30 -0400

    edited smart constructor test

--- src/test/scala/objektwerks/classes/SmartConstructorTest.scala
+++ src/test/scala/objektwerks/classes/SmartConstructorTest.scala
@@ -7,9 +7,9 @@ import org.scalatest.matchers.should.Matchers
   * See: https://tuleism.github.io/blog/2020/scala-smart-constructors/
   */
 final class SmartConstructorTest extends AnyFunSuite with Matchers:
+  // 1.
   sealed trait Email:
     def address: String
-
   object Email:
     def validate(newAddress: String): Option[Email] =
       if newAddress.contains("@") then
@@ -19,15 +19,15 @@ final class SmartConstructorTest extends AnyFunSuite with Matchers:
         )
       else None
 
+  // 2.
   sealed abstract case class Xmail private (address: String)
-
   object Xmail:
     def validate(newAddress: String): Option[Xmail] =
       if newAddress.contains("@") then Some( new Xmail(newAddress){} )
       else None
 
+  // 3.
   final case class Ymail private (address: String)
-  
   object Ymail:
     def validate(newAddress: String): Option[Ymail] =
       if newAddress.contains("@") then Some( Ymail(newAddress) )
