21b087b8ef100d622c30d494f95c1a5ed2244eba
objektwerks <objektwerks@runbox.com>
2024-01-20 09:32:14 -0500

    added infix to add and and methods

--- src/test/scala/objektwerks/types/PhantomTypeTest.scala
+++ src/test/scala/objektwerks/types/PhantomTypeTest.scala
@@ -10,11 +10,11 @@ class PhantomTypeTest extends AnyFunSuite with Matchers:
   final case class Code[A] (value: String)
 
   extension (intCode: Code[Int])
-    def add(that: Code[Int]): Code[Int] = Code(s"(${intCode.value} + ${that.value})")
+    infix def add(that: Code[Int]): Code[Int] = Code(s"(${intCode.value} + ${that.value})")
     def multiply(that: Code[Int]): Code[Int] = Code(s"(${intCode.value} * ${that.value})")
 
   extension (boolCode: Code[Boolean])
-    def and(that: Code[Boolean]): Code[Boolean] = Code(s"(${boolCode.value} && ${that.value})")
+    infix def and(that: Code[Boolean]): Code[Boolean] = Code(s"(${boolCode.value} && ${that.value})")
     def or(that: Code[Boolean]): Code[Boolean] = Code(s"(${boolCode.value} || ${that.value})")
 
   object Code:
