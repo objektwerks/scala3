6fb972adcbb339c533279c06b506bf39ef49aa64
objektwerks <objektwerks@runbox.com>
2025-05-02 10:26:06 -0400

    enhanced named tuple test

--- src/test/scala/objektwerks/collections/NamedTupleTest.scala
+++ src/test/scala/objektwerks/collections/NamedTupleTest.scala
@@ -3,6 +3,10 @@ package objektwerks.collections
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-final class TupleTest extends AnyFunSuite with Matchers:
+final class NamedTupleTest extends AnyFunSuite with Matchers:
+  type Person = (name: String, age: Int)
+
   test("named"):
-    "named".nonEmpty shouldBe true
+    val fred = (name = "Fred Flintstone", age = 68)
+    fred.name shouldBe "Fred Flintstone"
+    fred.age shouldBe 68
