fbe2d8a3ec4db0d8580199fa755eddf9fc58cad6
objektwerks <objektwerks@runbox.com>
2025-05-03 11:30:28 -0400

    edited app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -3,13 +3,7 @@ package objektwerks
 /**
  * No object required for Scala 3 app!
  *
- * To run via IDE:
- * 1. select run triangle ( in left margin ) > Modify Run Configuration...
- * 2. provide CLI args, such as: Fred Flintstone
- * 3. select run triangle > run 'greeting'
- *
- * To run via terminal:
- * 1. sbt "run Fred Flintstone"
+ * To run via terminal: sbt "run Fred Flintstone"
  */
 @main def greeting(first: String, last: String): Unit =
   println(s"@main greetings(first: String, last: String) => Greetings, $first $last!")
