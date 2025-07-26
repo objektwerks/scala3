7441314a6ced948ddfbc16ddba4df4a018155b45
objektwerks <objektwerks@runbox.com>
2025-07-26 11:41:25 -0400

    added magnet type test

---
+++ src/test/scala/objektwerks/types/MagnetTypeTest.scala
@@ -0,0 +1,8 @@
+package objektwerks.types
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+final class MagnetTypeTest extends AnyFunSuite with Matchers:
+  test("pattern"):
+    println("todo")
