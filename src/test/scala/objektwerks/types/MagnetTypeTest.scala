fa22939eaa2c34e60875d882247c837a28a9efad
objektwerks <objektwerks@runbox.com>
2025-07-27 08:41:06 -0400

    enhanced magnet type test

--- src/test/scala/objektwerks/types/MagnetTypeTest.scala
+++ src/test/scala/objektwerks/types/MagnetTypeTest.scala
@@ -3,6 +3,11 @@ package objektwerks.types
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
+sealed trait FutureMagnet:
+  type Result
+
+  def apply() : Result
+
 final class MagnetTypeTest extends AnyFunSuite with Matchers:
   test("pattern"):
     println("todo")
