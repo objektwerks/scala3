580e72dfd5f5012b373e45aa6aeede18f89d0a53
objektwerks <objektwerks@runbox.com>
2025-11-04 07:03:08 -0500

    corrected named tuple test

--- src/test/scala/objektwerks/collections/NamedTupleTest.scala
+++ src/test/scala/objektwerks/collections/NamedTupleTest.scala
@@ -35,8 +35,8 @@ final class NamedTupleTest extends AnyFunSuite with Matchers:
       .filter { person => person.age >= 18 }
       .map { person => NamedTuple("name" -> person.name, "age" -> ( person.age + 1 )) }
     seniors.length shouldBe 1
-    seniors.head.toTuple._1 shouldBe ("name", "Fred Flintstone")
-    seniors.head.toTuple._2 shouldBe ("age", 69)
+    seniors.head.toTuple._2._1 shouldBe ("name", "Fred Flintstone")
+    seniors.head.toTuple._2._2 shouldBe ("age", 69)
 
     val updatedFred = NamedTuple("name" -> fred.name, "age" -> (fred.age + 1))
     updatedFred.toTuple._1 shouldBe ("name", "Fred Flintstone")
