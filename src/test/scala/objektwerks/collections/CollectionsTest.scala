4db4b5b02b0c6cc1885c82c5efbd94911859506c
objektwerks <objektwerks@runbox.com>
2026-03-16 11:46:58 -0400

    added parens to methods

--- src/test/scala/objektwerks/collections/CollectionsTest.scala
+++ src/test/scala/objektwerks/collections/CollectionsTest.scala
@@ -17,9 +17,9 @@ final class CollectionsTest extends AnyFunSuite with Matchers:
     Seq.newBuilder[Int].addOne(1).addOne(2).addOne(3).result() shouldBe Seq(1, 2, 3)
     IndexedSeq.newBuilder[Int].addOne(1).addOne(2).addOne(3).result() shouldBe IndexedSeq(1, 2, 3)
     LinearSeq.newBuilder[Int].addOne(1).addOne(2).addOne(3).result() shouldBe LinearSeq(1, 2, 3)
-    Set.newBuilder[Int].addOne(1).addOne(2).addOne(3).result shouldBe Set(1, 2, 3)
-    Map.newBuilder[Int, Int].addOne(1 -> 1).addOne(2 -> 2).addOne(3 -> 3).result shouldBe Map(1 -> 1, 2 -> 2, 3 -> 3)
-    View.newBuilder[Int].addOne(1).addOne(2).addOne(3).result.toList shouldBe List(1, 2, 3)
+    Set.newBuilder[Int].addOne(1).addOne(2).addOne(3).result() shouldBe Set(1, 2, 3)
+    Map.newBuilder[Int, Int].addOne(1 -> 1).addOne(2 -> 2).addOne(3 -> 3).result() shouldBe Map(1 -> 1, 2 -> 2, 3 -> 3)
+    View.newBuilder[Int].addOne(1).addOne(2).addOne(3).result().toList shouldBe List(1, 2, 3)
     MapView.newBuilder[Int, Int].addOne(1 -> 1).addOne(2 -> 2).addOne(3 -> 3).result().toMap shouldBe Map(1 -> 1, 2 -> 2, 3 -> 3)
     LazyList.newBuilder[Int].addOne(1).addOne(2).addOne(3).result() shouldBe LazyList(1, 2, 3)
 
